ec5f02895e8ef2f6ba427809ebea1a4b989c8e99
Josh Peterson <joshuadanpeterson@gmail.com>
2024-02-29 15:48:57 -0700

    Disabled mock vim config

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@
 
 -- Custom configs
 require('config.settings') -- For basic Neovim settings
-require("config.vim")      -- For vim config
+-- require("config.vim")      -- For vim config
 
 -- lazy.nvim setup
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
