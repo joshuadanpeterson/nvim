5a30360eb9116c28c7a9281d67fd830cda1efb47
Josh Peterson <joshuadanpeterson@gmail.com>
2024-03-06 04:07:31 -0700

    Updated ChatGPT Keymaps
    Changed leader key

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -197,20 +197,20 @@ local gitMappings = {
 
 -- ChatGPT Keymaps
 local chatgptMappings = {
-	-- name = "ChatGPT Keymaps",
-	['cc'] = { "<cmd>ChatGPT<CR>", "ChatGPT" },
-	['ce'] = { "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction", mode = { "n", "v" } },
-	['cg'] = { "<cmd>ChatGPTRun grammar_correction<CR>", "Grammar Correction", mode = { "n", "v" } },
-	['ct'] = { "<cmd>ChatGPTRun translate<CR>", "Translate", mode = { "n", "v" } },
-	['ck'] = { "<cmd>ChatGPTRun keywords<CR>", "Keywords", mode = { "n", "v" } },
-	['cd'] = { "<cmd>ChatGPTRun docstring<CR>", "Docstring", mode = { "n", "v" } },
-	['ca'] = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests", mode = { "n", "v" } },
-	['co'] = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code", mode = { "n", "v" } },
-	['cs'] = { "<cmd>ChatGPTRun summarize<CR>", "Summarize", mode = { "n", "v" } },
-	['cf'] = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs", mode = { "n", "v" } },
-	['cx'] = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code", mode = { "n", "v" } },
-	['cr'] = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit", mode = { "n", "v" } },
-	['cl'] = { "<cmd>ChatGPTRun code_readability_analysis<CR>", "Code Readability Analysis", mode = { "n", "v" } },
+	name = "ChatGPT Keymaps",
+	['c'] = { "<cmd>ChatGPT<CR>", "ChatGPT" },
+	['e'] = { "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction", mode = { "n", "v" } },
+	['g'] = { "<cmd>ChatGPTRun grammar_correction<CR>", "Grammar Correction", mode = { "n", "v" } },
+	['t'] = { "<cmd>ChatGPTRun translate<CR>", "Translate", mode = { "n", "v" } },
+	['k'] = { "<cmd>ChatGPTRun keywords<CR>", "Keywords", mode = { "n", "v" } },
+	['d'] = { "<cmd>ChatGPTRun docstring<CR>", "Docstring", mode = { "n", "v" } },
+	['a'] = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests", mode = { "n", "v" } },
+	['o'] = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code", mode = { "n", "v" } },
+	['s'] = { "<cmd>ChatGPTRun summarize<CR>", "Summarize", mode = { "n", "v" } },
+	['f'] = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs", mode = { "n", "v" } },
+	['x'] = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code", mode = { "n", "v" } },
+	['r'] = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit", mode = { "n", "v" } },
+	['l'] = { "<cmd>ChatGPTRun code_readability_analysis<CR>", "Code Readability Analysis", mode = { "n", "v" } },
 }
 
 -- DAP Plugin Keymaps
@@ -247,7 +247,7 @@ wk.register(liveServerMappings, { prefix = "<leader>", mode = "n" })
 wk.register(tmuxTelescopeMappings, { prefix = "<leader>", mode = "n" })
 
 -- Registering ChatGPT Keymaps
-wk.register(chatgptMappings, { prefix = "<leader>" })
+wk.register(chatgptMappings, { prefix = "<leader>c" })
 
 -- Registering DAP mappings under the 'n' (normal) mode leader key
 -- Note: F-keys are registered globally, not under a leader key.
