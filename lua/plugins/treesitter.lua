7a820c6a2999f811ffda471f675d8014fec117ab
Josh Peterson <joshuadanpeterson@gmail.com>
2024-03-08 12:37:25 -0700

    Added nvim-treesitter-textobjects
    for syntax aware text-objects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -10,11 +10,61 @@ return {
         {
                 'nvim-treesitter/nvim-treesitter',
                 run = ':TSUpdate',
+                dependencies = {
+                        'nvim-treesitter/nvim-treesitter-textobjects',
+                },
         },
 
         -- nvim-treesitter/playground to explore Treesitter queries in a UI
         {
                 'nvim-treesitter/playground',
                 cmd = 'TSPlaygroundToggle',
+                }
+        },
+
+        -- nvim-treesitter-textobjects
+        {
+                'nvim-treesitter-textobjects',
+                lazy = true,
+                config = function()
+                        require('nvim-treesitter.configs').setup {
+                                textobjects = {
+                                        select = {
+                                                enable = true,
+
+                                                -- Automatically jump forward to textobj, similar to targets.vim
+                                                lookahead = true,
+
+                                                keymaps = {
+                                                        -- You can use the capture groups defined in textobjects.scm
+                                                        ['a='] = { query = '@assignment.outer', desc = 'Select outer part of an assignment' },
+                                                        ['i='] = { query = '@assignment.inner', desc = 'Select inner part of an assignment' },
+                                                        ['l='] = { query = '@assignment.lhs', desc = 'Select left hand side of an assignment' },
+                                                        ['r='] = { query = '@assignment.rhs', desc = 'Select right hand side of an assignment' },
+
+
+                                                        ['ai'] = { query = '@assignment.outer', desc = 'Select outer part of an conditional' },
+                                                        ['ii'] = { query = '@assignment.inner', desc = 'Select inner part of an conditional' },
+
+
+                                                        ['al'] = { query = '@assignment.outer', desc = 'Select outer part of a loop' },
+                                                        ['il'] = { query = '@assignment.inner', desc = 'Select inner part of a loop' },
+
+
+                                                        ['af'] = { query = '@assignment.outer', desc = 'Select outer part of a function call' },
+                                                        ['if'] = { query = '@assignment.inner', desc = 'Select inner part of a function call' },
+
+
+                                                        ['am'] = { query = '@assignment.outer', desc = 'Select outer part of a method/function def' },
+                                                        ['im'] = { query = '@assignment.inner', desc = 'Select inner part of a method/function def' },
+
+
+                                                        ['ac'] = { query = '@assignment.outer', desc = 'Select outer part of a class' },
+                                                        ['ic'] = { query = '@assignment.inner', desc = 'Select inner part of a class' },
+                                                }
+                                        }
+                                }
+                        }
+                end
         },
 }
