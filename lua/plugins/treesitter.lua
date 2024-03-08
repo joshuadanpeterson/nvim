c85b82b2298aac98e883f33c2f1010622547ecae
Josh Peterson <joshuadanpeterson@gmail.com>
2024-03-08 12:38:35 -0700

    Added treesitter dependency to playground
    Just in case treesitter needed to be installed as a dependency. May change later

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -19,6 +19,8 @@ return {
         {
                 'nvim-treesitter/playground',
                 cmd = 'TSPlaygroundToggle',
+                dependencies = {
+                        'nvim-treesitter/nvim-treesitter',
                 }
         },
 
