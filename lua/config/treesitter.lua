ba16fc5aabbd84c311031cc853a3499b3f69b88f
Josh Peterson <joshuadanpeterson@gmail.com>
2024-03-08 12:26:03 -0700

    Changed ensure_installed languages
    Edited languages down to core for treesitter to function properly. Everything else is auto_installed

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -8,7 +8,7 @@ require('plugins.treesitter')
 vim.defer_fn(function()
     require('nvim-treesitter.configs').setup {
         -- Add languages to be installed here that you want installed for treesitter
-        ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'json', 'css', 'markdown', 'markdown_inline' },
+        ensure_installed = { 'c', 'lua', 'vimdoc', 'vim' },
 
         -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
         auto_install = true,
