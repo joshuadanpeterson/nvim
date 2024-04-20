14e29353997289ad73b3ba80e03573a5633d90e8
Josh Peterson <joshuadanpeterson@gmail.com>
2024-04-20 03:09:39 -0600

    Updated config/lsp.lua
    Updated lsp config to remove mason-lspconfig error

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -4,210 +4,126 @@
 local lspconfig = require('lspconfig')
 local cmp_nvim_lsp = require('cmp_nvim_lsp')
 
--- LSP servers setup
-local servers = { 'lua_ls', 'tsserver', 'pyright', 'html', 'cssls', 'bashls', 'rust_analyzer', 'gopls', 'phpactor',
-    'ruby_ls', 'jsonls', 'yamlls', 'sqls', 'dockerls', 'vimls' }
-for _, lsp in ipairs(servers) do
-    lspconfig[lsp].setup {
-        capabilities = cmp_nvim_lsp.default_capabilities(),
-    }
-end
+-- Enhanced capabilities from nvim-cmp for LSP
+local capabilities = cmp_nvim_lsp.default_capabilities()
 
--- lspconfig
-require 'lspconfig'.lua_ls.setup {
-    on_init = function(client)
-        local path = client.workspace_folders[1].name
-        if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
-            client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
-                lua = {
-                    runtime = {
-                        -- tell the language server which version of lua you're using
-                        -- (most likely luajit in the case of neovim)
-                        version = 'luaJIT'
-                    },
-                    diagnostics = {
-                        globals = { 'vim' }, -- This informs the LS that `vim` is a global variable and shouldn't be flagged as undefined
-                    },
-                    -- make the server aware of neovim runtime files
-                    workspace = {
-                        checkthirdparty = false,
-                        library = vim.api.nvim_get_runtime_file("", true), -- Make the LS aware of Neovim runtime files
-                    },
-
-                }
-            })
-
-            client.notify("workspace/didchangeconfiguration", { settings = client.config.settings })
-        end
-        return true
-    end
-}
-
--- [[ Configure LSP ]]
---  This function gets run when an LSP connects to a particular buffer.
-local on_attach = function(_, bufnr)
-    -- Key mappings or other buffer-specific settings go here
-    -- Example of setting a keymap with bufnr:
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>',
-        { noremap = true, silent = true })
-    -- ... more configurations
-end
--- NOTE: Remember that lua is a real programming language, and as such it is possible
--- to define small helper and utility functions so you don't have to repeat yourself
--- many times.
-
--- mason-lspconfig requires that these setup functions are called in this order
--- before setting up the servers.
+-- Setup mason to manage LSP installations
 require('mason').setup()
-require('mason-lspconfig').setup()
+require('mason-lspconfig').setup({
+    ensure_installed = {
+        'lua_ls', 'tsserver', 'pyright', 'html', 'cssls', 'bashls', 'rust_analyzer',
+        'gopls', 'phpactor', 'solargraph', 'jsonls', 'yamlls', 'sqls', 'dockerls', 'vimls',
+        -- Include additional servers here
+    },
+})
 
--- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-
--- Load Neovim's built-in LSP configuration module
-local lspconfig = require('lspconfig')
-
--- Configure Jedi Language Server
-lspconfig.jedi_language_server.setup {
-    on_attach = on_attach, -- assuming you have an `on_attach` function as shown in your config
-    flags = {
-        debounce_text_changes = 150,
-    }
-}
-
--- TypeScript and JavaScript LSP configuration
-lspconfig.tsserver.setup {
-    on_attach = function(_client, _bufnr)
-        -- Your custom on_attach function
-        -- (e.g., key mappings for LSP functions)
-    end,
-    -- Additional LSP settings can be added here
-}
-
---
---  Add any additional override configuration in the following tables. They will be passed to
---  the `settings` field of the server config. You must look up that documentation yourself.
---
---  If you want to override the default filetypes that your language server will attach to you can
---  define the property 'filetypes' to the map in question.
+-- LSP servers setup with specific configurations for certain servers
 local servers = {
-    -- clangd = {},
-    -- gopls = {},
-    -- pyright = {},
-    -- rust_analyzer = {},
-    -- tsserver = {},
-    -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+    'lua_ls', 'tsserver', 'pyright', 'html', 'cssls', 'bashls', 'rust_analyzer',
+    'gopls', 'phpactor', 'solargraph', 'jsonls', 'yamlls', 'sqls', 'dockerls', 'vimls',
+}
 
+-- Special configurations for specific LSP servers
+local special_configurations = {
     lua_ls = {
-        Lua = {
-            workspace = { checkThirdParty = false },
-            telemetry = { enable = false },
-            -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-            -- diagnostics = { disable = { 'missing-fields' } },
+        settings = {
+            Lua = {
+                workspace = {
+                    checkThirdParty = false,
+                    library = vim.api.nvim_get_runtime_file("", true),
+                },
+                telemetry = { enable = false },
+            },
         },
+        on_init = function(client)
+            client.config.settings = vim.tbl_deep_extend('force', client.config.settings or {}, {
+                Lua = {
+                    runtime = { version = 'LuaJIT' },
+                    diagnostics = { globals = { 'vim' } },
+                },
+            })
+            client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+            return true
+        end,
     },
 }
 
+-- Function to set up LSP servers
+local function setup_servers()
+    for _, server in ipairs(servers) do
+        local config = special_configurations[server] or {}
+        config.capabilities = capabilities
+        lspconfig[server].setup(config)
+    end
+end
 
--- Setup LSP Configurations for `nvim-cmp`
--- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+-- Configure LSP servers
+setup_servers()
 
--- Ensure the servers above are installed
-local mason_lspconfig = require 'mason-lspconfig'
-
-mason_lspconfig.setup {
-    ensure_installed = vim.tbl_keys(servers),
-}
-
-mason_lspconfig.setup_handlers {
-    function(server_name)
-        require('lspconfig')[server_name].setup {
-            capabilities = capabilities,
-            on_attach = on_attach,
-            settings = servers[server_name],
-            filetypes = (servers[server_name] or {}).filetypes,
-        }
-    end,
-}
-
--- [[ Configure nvim-cmp ]]
--- See `:help cmp`
-local cmp = require 'cmp'
-local luasnip = require 'luasnip'
+-- Configure nvim-cmp for auto-completion
+local cmp = require('cmp')
+local luasnip = require('luasnip')
 require('luasnip.loaders.from_vscode').lazy_load()
-luasnip.config.setup {}
 
-cmp.setup {
+cmp.setup({
     snippet = {
         expand = function(args)
             luasnip.lsp_expand(args.body)
         end,
     },
     completion = {
-        completeopt = 'menu,menuone,noinsert'
+        completeopt = 'menu,menuone,noinsert',
     },
-    mapping = cmp.mapping.preset.insert {
+    mapping = cmp.mapping.preset.insert({
         ['<C-n>'] = cmp.mapping.select_next_item(),
         ['<C-p>'] = cmp.mapping.select_prev_item(),
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete {},
-        ['<CR>'] = cmp.mapping.confirm {
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping.confirm({
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
-        },
-        ['<Tab>'] = cmp.mapping(function(fallback)
+        }),
+        ['<Tab>'] = function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
-            elseif luasnip.expand_or_locally_jumpable() then
+            elseif luasnip.expand_or_jumpable() then
                 luasnip.expand_or_jump()
             else
                 fallback()
             end
-        end, { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
+        end,
+        ['<S-Tab>'] = function(fallback)
             if cmp.visible() then
                 cmp.select_prev_item()
-            elseif luasnip.locally_jumpable(-1) then
+            elseif luasnip.jumpable(-1) then
                 luasnip.jump(-1)
             else
                 fallback()
             end
-        end, { 'i', 's' }),
-    },
+        end,
+    }),
     sources = {
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
     },
-}
+})
 
--- Example: Setting up Lua LSP
-require 'lspconfig'.lua_ls.setup {
-    capabilities = capabilities,
-}
-
--- `/` cmdline setup.
+-- Cmdline setup for '/' and ':'
 cmp.setup.cmdline('/', {
     mapping = cmp.mapping.preset.cmdline(),
     sources = {
         { name = 'buffer' }
-    }
+    },
 })
 
--- `:` cmdline setup.
 cmp.setup.cmdline(':', {
     mapping = cmp.mapping.preset.cmdline(),
     sources = cmp.config.sources({
         { name = 'path' }
     }, {
-        {
-            name = 'cmdline',
-            option = {
-                ignore_cmds = { 'Man', '!' }
-            }
-        }
-    })
+        { name = 'cmdline' }
+    }),
 })
 
 -- Integrates autopairs into Autocompletion
