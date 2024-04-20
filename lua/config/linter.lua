4d4a21ff1b5c18e377f98178f8fa0b4beaa0b360
Josh Peterson <joshuadanpeterson@gmail.com>
2024-04-20 03:07:11 -0600

    Updated linter
    Changed JavaScript and TypeScript linter from eslint to eslint_d

--- lua/config/linter.lua
+++ lua/config/linter.lua
@@ -3,8 +3,8 @@
 -- configure 'nvim-lint'
 require('lint').linters_by_ft = {
     lua = { 'luacheck' },
-    javascript = { 'eslint' },
-    typescript = { 'eslint' },
+    javascript = { 'eslint_d' },
+    typescript = { 'eslint_d' },
     python = { 'flake8', 'pylint', 'mypy' },
     html = { 'htmlhint' },
     css = { 'stylelint' },
