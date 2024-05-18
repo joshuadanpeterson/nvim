92b912e439aacb99948f7f3023cebf04a1d7bc66
Josh Peterson <joshuadanpeterson@gmail.com>
2024-05-18 00:00:38 -0600

    Added nvim-surround and vim-jsx-improve
    Added nvim-surround to surround text with {, [, (, <, ", ', ` | Also
    added jsx syntax highlighting

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -65,5 +65,22 @@ return {
         },
       })
     end,
-  }
+  },
+
+  -- nvim-surround: surround selections
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  },
+
+  -- For JSX syntax highlighting
+  {
+    "neoclide/vim-jsx-improve",
+  },
 }
