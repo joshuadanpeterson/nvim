b93c6abf01cbc6aee558c8d5280ddf0140e98aa4
Josh Peterson <joshuadanpeterson@gmail.com>
2024-05-18 00:02:45 -0600

    Switched overrides to true
    At the recommendation of :checkhealth

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -4,37 +4,28 @@
 return {
         -- Plugin identifier and lazy loading configuration.
         'folke/noice.nvim',
-        event = 'VeryLazy', -- Specifies that the plugin should load at a very late stage of the startup process.
-
-        -- Dependencies required by `noice.nvim` for it to work.
+        event = 'VeryLazy',
         dependencies = {
-                "MunifTanjim/nui.nvim", -- UI component library needed for creating UI elements.
-                "rcarriga/nvim-notify", -- Notification manager for displaying messages in a more visually appealing manner.
+                "MunifTanjim/nui.nvim",
+                "rcarriga/nvim-notify",
         },
-
-        -- Configuration function for setting up `noice.nvim`.
         config = function()
                 require('noice').setup({
-                        -- Command line configuration section.
                         cmdline = {
-                                enabled = true,                  -- Enables the enhanced command line UI provided by Noice.
-                                view = "cmdline_popup",          -- Uses a popup window for the command line to replace the default at the bottom.
+                                enabled = true,
+                                view = "cmdline_popup",
                                 opts = {
-                                        relative = "editor",     -- Make the window relative to the editor's dimensions
-                                        position = "50%",        -- Attempt to position in the center (this is a simplistic approach)
-                                        size = {                 -- Define the size of the cmdline popup
-                                                width = 60,      -- Set the width (you can adjust this to your liking)
-                                                height = "auto", -- Let the height adjust automatically based on the content
+                                        relative = "editor",
+                                        position = "50%",
+                                        size = {
+                                                width = 60,
+                                                height = "auto",
                                         },
                                         border = {
-                                                style =
-                                                "single" -- Define the border style, options include "single", "double", "rounded", "solid", or "shadow"
+                                                style = "single"
                                         },
                                 },
-
-                                -- Defines custom formats for different command line scenarios.
                                 format = {
-                                        -- Each table entry customizes how certain types of command line inputs are displayed.
                                         cmdline = { pattern = "^:", icon = "", lang = "vim" },
                                         search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
                                         search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
@@ -44,33 +35,24 @@ return {
                                         input = {},
                                 },
                         },
-
-                        -- Messages configuration section.
                         messages = {
-                                enabled = true,              -- Enables the enhanced messages UI, allowing for better message management.
-                                view = "notify",             -- Uses `nvim-notify` for displaying messages.
-                                view_error = "notify",       -- Uses `nvim-notify` for displaying error messages.
-                                view_warn = "notify",        -- Uses `nvim-notify` for displaying warning messages.
-                                view_history = "notify",     -- Uses the default messages view for displaying message history.
-                                view_search = "virtualtext", -- Uses virtual text for displaying search count messages.
+                                enabled = true,
+                                view = "notify",
+                                view_error = "notify",
+                                view_warn = "notify",
+                                view_history = "notify",
+                                view_search = "virtualtext",
                         },
-
-                        -- Popup menu configuration section.
                         popupmenu = {
-                                enabled = true,  -- Enables the enhanced popup menu UI.
-                                backend = "nui", -- Specifies `nui.nvim` as the backend for displaying the popup menu.
-                                kind_icons = {}, -- Configuration for icons next to completion items, if desired.
+                                enabled = true,
+                                backend = "nui",
+                                kind_icons = {},
                         },
-
-                        -- Configuration for redirecting command output or messages.
                         redirect = {
-                                view = "popup",                  -- Uses a popup view for displaying redirected messages.
-                                filter = { event = "msg_show" }, -- Filters for specific types of messages to redirect.
+                                view = "popup",
+                                filter = { event = "msg_show" },
                         },
-
-                        -- Custom commands specific to `noice.nvim`.
                         commands = {
-                                -- This section allows for configuring custom commands that enhance message viewing and management.
                                 history = {
                                         view = "split",
                                         opts = { enter = true, format = "details" },
@@ -105,17 +87,12 @@ return {
                                         filter_opts = { reverse = true },
                                 },
                         },
-
-                        -- Notification integration configuration.
                         notify = {
-                                enabled = true,  -- Enables using `noice.nvim` for `vim.notify`, integrating with `nvim-notify`.
-                                view = "notify", -- Specifies that notifications should be displayed using `nvim-notify`.
+                                enabled = true,
+                                view = "notify",
                         },
-
-                        -- Language Server Protocol (LSP) configurations.
                         lsp = {
                                 progress = {
-                                        -- Configurations for displaying LSP progress messages.
                                         enabled = true,
                                         format = "lsp_progress",
                                         format_done = "lsp_progress_done",
@@ -123,20 +100,17 @@ return {
                                         view = "mini",
                                 },
                                 override = {
-                                        -- Allows for overriding default LSP formatting functions.
-                                        ["vim.lsp.util.convert_input_to_markdown_lines"] = false,
-                                        ["vim.lsp.util.stylize_markdown"] = false,
-                                        ["cmp.entry.get_documentation"] = false,
+                                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                                        ["vim.lsp.util.stylize_markdown"] = true,
+                                        ["cmp.entry.get_documentation"] = true,
                                 },
                                 hover = {
-                                        -- Configurations for LSP hover documentation.
                                         enabled = true,
                                         silent = false,
                                         view = nil,
                                         opts = {},
                                 },
                                 signature = {
-                                        -- Configurations for function signature help.
                                         enabled = true,
                                         auto_open = {
                                                 enabled = true,
@@ -148,13 +122,11 @@ return {
                                         opts = {},
                                 },
                                 message = {
-                                        -- Configurations for displaying LSP server messages.
                                         enabled = true,
                                         view = "notify",
                                         opts = {},
                                 },
                                 documentation = {
-                                        -- Configurations for the appearance and behavior of LSP documentation.
                                         view = "hover",
                                         opts = {
                                                 lang = "markdown",
@@ -166,7 +138,6 @@ return {
                                 },
                         },
                         markdown = {
-                                -- Configurations for handling markdown and links within hover documentation.
                                 hover = {
                                         ["|(%S-)|"] = vim.cmd.help,
                                         ["%[.-%]%((%S-)%)"] = require("noice.util").open,
@@ -181,26 +152,24 @@ return {
                                 },
                         },
                         health = {
-                                checker = true, -- Enables running health checks, part of `noice.nvim`'s diagnostics.
+                                checker = true,
                         },
                         smart_move = {
-                                -- Configurations for automatically moving UI elements to avoid overlapping with existing ones.
                                 enabled = true,
                                 excluded_filetypes = { "cmp_menu", "cmp_docs", "notify" },
                         },
                         presets = {
-                                -- Allows for enabling, disabling, or configuring presets that modify UI behavior.
                                 bottom_search = false,
                                 command_palette = true,
                                 long_message_to_split = false,
                                 inc_rename = false,
                                 lsp_doc_border = false,
                         },
-                        throttle = 1000 / 30, -- Defines the frequency for UI updates, improving performance.
-                        views = {},           -- Configuration for custom views.
-                        routes = {},          -- Configuration for routing messages through different views or filters.
-                        status = {},          -- Configuration for integrating with the status line.
-                        format = {},          -- Configuration for custom formatting of messages and inputs.
+                        throttle = 1000 / 30,
+                        views = {},
+                        routes = {},
+                        status = {},
+                        format = {},
                 })
-        end
+        end,
 }
