4d9ec6efe27b59b128465641ad0a65c3ad36ffcc
Josh Peterson <joshuadanpeterson@gmail.com>
2024-05-18 21:38:29 -0600

    Added window border and completion config

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -43,6 +43,13 @@ cmp.setup({
             luasnip.lsp_expand(args.body)
         end,
     },
+    completion = {
+        completeopt = 'menu,menuone,noinsert',
+    },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
     mapping = cmp.mapping.preset.insert({
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
