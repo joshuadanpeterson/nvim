d48bb5930b5169690c15fe6bf306c5632cdab56f
Josh Peterson <joshuadanpeterson@gmail.com>
2024-05-18 20:34:28 -0600

    Added config for emmet-ls
    Deleted emmet-language-server

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -8,6 +8,9 @@ local null_ls = require('null-ls')
 
 -- Enhanced capabilities from nvim-cmp for LSP
 local capabilities = cmp_nvim_lsp.default_capabilities()
+-- For emmet-ls
+local capabiliteez = vim.lsp.protocol.make_client_capabilities()
+capabiliteez.textDocument.completion.completionItem.snippetSupport = true
 
 -- Setup mason to manage LSP installations
 require('mason').setup()
@@ -87,30 +90,19 @@ lspconfig.tsserver.setup({
     capabilities = capabilities,
 })
 
--- Configure emmet_language_server
-lspconfig.emmet_language_server.setup({
-    filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "pug", "typescriptreact" },
-    -- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
-    -- **Note:** only the options listed in the table are supported.
+lspconfig.emmet_ls.setup({
+    -- on_attach = on_attach,
+    capabilities = capabiliteez,
+    filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less', 'javascript' },
     init_options = {
-        ---@type table<string, string>
-        includeLanguages = {},
-        --- @type string[]
-        excludeLanguages = {},
-        --- @type string[]
-        extensionsPath = {},
-        --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
-        preferences = {},
-        --- @type boolean Defaults to `true`
-        showAbbreviationSuggestions = true,
-        --- @type "always" | "never" Defaults to `"always"`
-        showExpandedAbbreviation = "always",
-        --- @type boolean Defaults to `false`
-        showSuggestionsAsSnippets = false,
-        --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
-        syntaxProfiles = {},
-        --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
-        variables = {},
+        html = {
+            options = {
+                -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+                ["bem.enabled"] = true,
+            },
+        },
+    }
+})
     },
 })
 
