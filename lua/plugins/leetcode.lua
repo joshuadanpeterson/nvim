01e4c3b0751e5868e4befb7c42bb88392121f244
Josh Peterson <joshuadanpeterson@gmail.com>
2024-05-20 23:43:35 -0600

    Updated plugins/leetcode.lua
    Linter changed " to '

--- lua/plugins/leetcode.lua
+++ lua/plugins/leetcode.lua
@@ -2,107 +2,107 @@
 -- Plugin config for leetcode.nvim
 
 return {
-        {
-                "kawre/leetcode.nvim",
-                build = ":TSUpdate html",
-                cmd = "Leet",
-                dependencies = {
-                        "nvim-telescope/telescope.nvim",
-                        "nvim-lua/plenary.nvim", -- required by telescope
-                        "MunifTanjim/nui.nvim",
+  {
+    'kawre/leetcode.nvim',
+    build = ':TSUpdate html',
+    cmd = 'Leet',
+    dependencies = {
+      'nvim-telescope/telescope.nvim',
+      'nvim-lua/plenary.nvim', -- required by telescope
+      'MunifTanjim/nui.nvim',
 
-                        -- optional
-                        "nvim-treesitter/nvim-treesitter",
-                        "rcarriga/nvim-notify",
-                        "nvim-tree/nvim-web-devicons",
-                },
-                opts = {
-                        {
-                                ---@type string
-                                arg = "leetcode.nvim",
+      -- optional
+      'nvim-treesitter/nvim-treesitter',
+      'rcarriga/nvim-notify',
+      'kyazdani42/nvim-web-devicons',
+    },
+    opts = {
+      {
+        ---@type string
+        arg = 'leetcode.nvim',
 
-                                ---@type lc.lang
-                                lang = "python3",
+        ---@type lc.lang
+        lang = 'python3',
 
-                                cn = { -- leetcode.cn
-                                        enabled = false, ---@type boolean
-                                        translator = true, ---@type boolean
-                                        translate_problems = true, ---@type boolean
-                                },
+        cn = { -- leetcode.cn
+          enabled = false, ---@type boolean
+          translator = true, ---@type boolean
+          translate_problems = true, ---@type boolean
+        },
 
-                                ---@type lc.storage
-                                storage = {
-                                        home = vim.fn.stdpath("data") .. "/leetcode",
-                                        cache = vim.fn.stdpath("cache") .. "/leetcode",
-                                },
+        ---@type lc.storage
+        storage = {
+          home = vim.fn.stdpath 'data' .. '/leetcode',
+          cache = vim.fn.stdpath 'cache' .. '/leetcode',
+        },
 
-                                ---@type table<string, boolean>
-                                plugins = {
-                                        non_standalone = false,
-                                },
+        ---@type table<string, boolean>
+        plugins = {
+          non_standalone = false,
+        },
 
-                                ---@type boolean
-                                logging = true,
+        ---@type boolean
+        logging = true,
 
-                                injector = {}, ---@type table<lc.lang, lc.inject>
+        injector = {}, ---@type table<lc.lang, lc.inject>
 
-                                cache = {
-                                        update_interval = 60 * 60 * 24 * 7, ---@type integer 7 days
-                                },
+        cache = {
+          update_interval = 60 * 60 * 24 * 7, ---@type integer 7 days
+        },
 
-                                console = {
-                                        open_on_runcode = true, ---@type boolean
+        console = {
+          open_on_runcode = true, ---@type boolean
 
-                                        dir = "row", ---@type lc.direction
+          dir = 'row', ---@type lc.direction
 
-                                        size = { ---@type lc.size
-                                                width = "90%",
-                                                height = "75%",
-                                        },
+          size = { ---@type lc.size
+            width = '90%',
+            height = '75%',
+          },
 
-                                        result = {
-                                                size = "60%", ---@type lc.size
-                                        },
+          result = {
+            size = '60%', ---@type lc.size
+          },
 
-                                        testcase = {
-                                                virt_text = true, ---@type boolean
+          testcase = {
+            virt_text = true, ---@type boolean
 
-                                                size = "40%", ---@type lc.size
-                                        },
-                                },
+            size = '40%', ---@type lc.size
+          },
+        },
 
-                                description = {
-                                        position = "left", ---@type lc.position
+        description = {
+          position = 'left', ---@type lc.position
 
-                                        width = "40%", ---@type lc.size
+          width = '40%', ---@type lc.size
 
-                                        show_stats = true, ---@type boolean
-                                },
+          show_stats = true, ---@type boolean
+        },
 
-                                hooks = {
-                                        ---@type fun()[]
-                                        ["enter"] = {},
+        hooks = {
+          ---@type fun()[]
+          ['enter'] = {},
 
-                                        ---@type fun(question: lc.ui.Question)[]
-                                        ["question_enter"] = {},
+          ---@type fun(question: lc.ui.Question)[]
+          ['question_enter'] = {},
 
-                                        ---@type fun()[]
-                                        ["leave"] = {},
-                                },
+          ---@type fun()[]
+          ['leave'] = {},
+        },
 
-                                keys = {
-                                        toggle = { "q", "<Esc>" }, ---@type string|string[]
-                                        confirm = { "<CR>" }, ---@type string|string[]
+        keys = {
+          toggle = { 'q', '<Esc>' }, ---@type string|string[]
+          confirm = { '<CR>' }, ---@type string|string[]
 
-                                        reset_testcases = "r", ---@type string
-                                        use_testcase = "U", ---@type string
-                                        focus_testcases = "H", ---@type string
-                                        focus_result = "L", ---@type string
-                                },
+          reset_testcases = 'r', ---@type string
+          use_testcase = 'U', ---@type string
+          focus_testcases = 'H', ---@type string
+          focus_result = 'L', ---@type string
+        },
 
-                                ---@type boolean
-                                image_support = false,
-                        }
-                },
-        }
+        ---@type boolean
+        image_support = false,
+      },
+    },
+  },
 }
