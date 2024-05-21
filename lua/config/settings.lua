0fcb09dbb219d486b379ee756d9611b98cad83e2
Josh Peterson <joshuadanpeterson@gmail.com>
2024-05-20 23:37:19 -0600

    Kept the same

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -101,11 +101,11 @@ vim.api.nvim_create_autocmd({ 'bufwritepost' }, {
 -- see `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('yankhighlight', { clear = true })
 vim.api.nvim_create_autocmd('textyankpost', {
-    callback = function()
-        vim.highlight.on_yank()
-    end,
-    group = highlight_group,
-    pattern = '*',
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
 })
 
 -- Set color consistency w/Tmux
