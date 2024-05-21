cc9d3ea1072beca7ebb700a5b54ffb03f564b78a
Josh Peterson <joshuadanpeterson@gmail.com>
2024-05-20 23:51:01 -0600

    Updated config/init.lua
    Linter removed () from require statements, etc

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,27 +1,28 @@
 -- Load config files
 
-
 -- Custom configs
-local keymaps = require('config.keymaps')     -- For keybindings managed with which-key
-local cmp = require('config.cmp')             -- For autocomplete config
-local tmux = require('config.tmux')           -- For Tmux navigation
-local conform = require('config.conform')     -- For conform.nvim
-local telescope = require('config.telescope') -- For Telescope
-local lsp = require('config.lsp')             -- For LSP configurations
-local linter = require('config.linter')       -- For linter configurations
-local fidget = require('config.fidget')       -- For fidget configurations
-local noice = require('config.noice')
-local lualine = require('config.lualine')
+local keymaps = require 'config.keymaps' -- For keybindings managed with which-key
+local cmp = require 'config.cmp' -- For autocomplete config
+local tmux = require 'config.tmux' -- For Tmux navigation
+local conform = require 'config.conform' -- For conform.nvim
+local telescope = require 'config.telescope' -- For Telescope
+local lsp = require 'config.lsp' -- For LSP configurations
+local linter = require 'config.linter' -- For linter configurations
+local fidget = require 'config.fidget' -- For fidget configurations
+local noice = require 'config.noice'
+local lualine = require 'config.lualine'
+local nvim_lint = require 'config.nvim-lint'
 
 return {
-    keymaps,
-    cmp,
-    tmux,
-    conform,
-    telescope,
-    lsp,
-    linter,
-    fidget,
-    noice,
-    lualine,
+  keymaps,
+  cmp,
+  tmux,
+  conform,
+  telescope,
+  lsp,
+  linter,
+  fidget,
+  noice,
+  lualine,
+  nvim_lint,
 }
