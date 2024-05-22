b44c78f37700c95b4f7d763008df2b82be8fb8fc
Josh Peterson <joshuadanpeterson@gmail.com>
2024-05-21 23:48:11 -0600

    Linter updated syntax
    Changed " to '

--- lua/config/conform.lua
+++ lua/config/conform.lua
@@ -1,21 +1,21 @@
 -- conform.nvim config: for formatting
 
-require("conform").setup({
+require('conform').setup {
   formatters_by_ft = {
-    lua = { "stylua" },
-    javascript = { "prettier" },  -- Assuming this covers TypeScript, JSON, YAML, HTML, CSS as well
+    lua = { 'stylua' },
+    javascript = { 'prettier' }, -- Assuming this covers TypeScript, JSON, YAML, HTML, CSS as well
     python = function(bufnr)
       -- Using a function to dynamically select the formatter, but keeping it simple for now
-      return { "black" }
+      return { 'black' }
     end,
-    go = { "gofumpt", "goimports" },  -- Running gofumpt first, then goimports
-    php = { "php-cs-fixer" },
-    ruby = { "rufo" },
-    sql = { "sqlformat" },
-    dockerfile = { "dockerfile_lint" },
-    vim = { "vim-codefmt" },  -- Assuming vim script files are recognized with 'vim' filetype
-    ["*"] = { "codespell" },
-    ["_"] = { "trim_whitespace" },
+    go = { 'gofumpt', 'goimports' }, -- Running gofumpt first, then goimports
+    php = { 'php-cs-fixer' },
+    ruby = { 'rufo' },
+    sql = { 'sqlformat' },
+    dockerfile = { 'dockerfile_lint' },
+    vim = { 'vim-codefmt' }, -- Assuming vim script files are recognized with 'vim' filetype
+    -- ["*"] = { "codespell" },
+    ['_'] = { 'trim_whitespace' },
   },
   format_on_save = {
     lsp_fallback = true,
@@ -29,15 +29,15 @@ require("conform").setup({
   formatters = {
     -- Add or modify custom formatter configurations here if needed
   },
-})
+}
 
 -- Direct assignments for quick overrides or additions
-require("conform").formatters_by_ft.lua = { "stylua" }
-require("conform").formatters_by_ft.javascript = { "prettier" }
-require("conform").formatters_by_ft.python = { "black" }
-require("conform").formatters_by_ft.go = { "gofumpt", "goimports" }
-require("conform").formatters_by_ft.php = { "php-cs-fixer" }
-require("conform").formatters_by_ft.ruby = { "rufo" }
-require("conform").formatters_by_ft.sql = { "sqlformat" }
-require("conform").formatters_by_ft.dockerfile = { "dockerfile_lint" }
-require("conform").formatters_by_ft.vim = { "vim-codefmt" }
+require('conform').formatters_by_ft.lua = { 'stylua' }
+require('conform').formatters_by_ft.javascript = { 'prettier' }
+require('conform').formatters_by_ft.python = { 'black' }
+require('conform').formatters_by_ft.go = { 'gofumpt', 'goimports' }
+require('conform').formatters_by_ft.php = { 'php-cs-fixer' }
+require('conform').formatters_by_ft.ruby = { 'rufo' }
+require('conform').formatters_by_ft.sql = { 'sqlformat' }
+require('conform').formatters_by_ft.dockerfile = { 'dockerfile_lint' }
+require('conform').formatters_by_ft.vim = { 'vim-codefmt' }
--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -3,96 +3,104 @@
     Customizes the status line in Neovim using the Lualine plugin. Configurations typically involve setting the appearance of the status line, choosing which components to display, and possibly integrating with other plugins for status information.
 ]]
 
+-- Setup nonicons
+local icons = require 'nvim-nonicons'
+local icon = icons.get 'git-branch'
+local nonicons_extention = require 'nvim-nonicons.extentions.lualine'
+local nonicons = nonicons_extention.mode
+
 local function is_active()
-    local ok, hydra = pcall(require, 'hydra.statusline')
-    return ok and hydra.is_active()
+  local ok, hydra = pcall(require, 'hydra.statusline')
+  return ok and hydra.is_active()
 end
 
 local function get_name()
-    local ok, hydra = pcall(require, 'hydra.statusline')
-    if ok then
-        return hydra.get_name()
-    end
-    return ''
+  local ok, hydra = pcall(require, 'hydra.statusline')
+  if ok then
+    return hydra.get_name()
+  end
+  return ''
 end
 
-local branch = { "branch", icon = "" }
-local mode = { "mode", icon = "󰡛" }
-local diagnostics = { "diagnostics", symbols = { error = " ", warn = " ", info = " " }, colored = false }
+local branch = { 'branch', icon = '' }
+local mode = { 'mode', icon = '󰡛' }
+local diagnostics = { 'diagnostics', symbols = { error = ' ', warn = ' ', info = ' ' }, colored = false }
 local searchcount = { 'searchcount', maxcount = 999, timeout = 500 }
 local datetime = {
-    'datetime',
-    fmt = function()
-        return os.date('%Y-%m-%d %H:%M')
-    end,
+  'datetime',
+  fmt = function()
+    return os.date '%Y-%m-%d %H:%M'
+  end,
 }
 
 local filetype = { 'filetype', colored = true }
 
 local copilot = {
-    'copilot',
-    symbols = {
-        status = {
-            icons = {
-                enabled = " ",
-                sleep = " ",
-                disabled = " ",
-                warning = " ",
-                unknown = " ",
-            },
-            hl = {
-                enabled = "#50FA7B",
-                sleep = "#AEB7D0",
-                disabled = "#6272A4",
-                warning = "#FFB86C",
-                unknown = "#FF5555"
-            }
-        },
-        spinners = require("copilot-lualine.spinners").dots,
-        spinner_color = "#6272A4"
+  'copilot',
+  symbols = {
+    status = {
+      icons = {
+        enabled = ' ',
+        sleep = ' ',
+        disabled = ' ',
+        warning = ' ',
+        unknown = ' ',
+      },
+      hl = {
+        enabled = '#50FA7B',
+        sleep = '#AEB7D0',
+        disabled = '#6272A4',
+        warning = '#FFB86C',
+        unknown = '#FF5555',
+      },
     },
-    show_colors = true,
-    show_loading = true
+    spinners = require('copilot-lualine.spinners').dots,
+    spinner_color = '#6272A4',
+  },
+  show_colors = true,
+  show_loading = true,
 }
 
-local Harpoonline = require("harpoonline")
-Harpoonline.setup({
-    on_update = function() require("lualine").refresh() end,
-})
-local harpoonline = { Harpoonline.format, "filename" }
+local Harpoonline = require 'harpoonline'
+Harpoonline.setup {
+  on_update = function()
+    require('lualine').refresh()
+  end,
+}
+local harpoonline = { Harpoonline.format, 'filename' }
 
 -- Define the lualine setup configuration
 local config = {
-    options = {
-        icons_enabled = true,
-        theme = 'nord',
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
+  options = {
+    icons_enabled = true,
+    theme = 'nord',
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
+  },
+  sections = {
+    lualine_a = { nonicons, mode },
+    lualine_b = {
+      { 'filename', file_status = true, path = 1 },
+      diagnostics,
+      branch,
+      'diff',
+      { get_name, cond = is_active },
+      -- 'grapple',
     },
-    sections = {
-        lualine_a = { mode },
-        lualine_b = {
-            { 'filename', file_status = true, path = 1 },
-            diagnostics,
-            branch,
-            'diff',
-            { get_name,   cond = is_active },
-            -- 'grapple',
-        },
-        lualine_c = { harpoonline, searchcount },
-        lualine_x = { 'encoding', filetype, copilot },
-        lualine_y = { 'progress', 'location' },
-        lualine_z = { datetime },
-    },
-    inactive_sections = {
-        lualine_a = { { 'filename', file_status = true, path = 1 }, 'diagnostics', 'diff' },
-        lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = { 'copilot' },
-        lualine_z = { 'location' }
-    },
-    tabline = {},
+    lualine_c = { harpoonline, searchcount },
+    lualine_x = { 'encoding', filetype, copilot },
+    lualine_y = { 'progress', 'location' },
+    lualine_z = { icon, datetime },
+  },
+  inactive_sections = {
+    lualine_a = { { 'filename', file_status = true, path = 1 }, 'diagnostics', 'diff' },
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = { 'copilot' },
+    lualine_z = { 'location' },
+  },
+  tabline = {},
 }
 
 -- Setup lualine with the defined configuration
