ae7428f9290b36c46d1b6db68fd17e5caf8ec685
Josh Peterson <joshuadanpeterson@gmail.com>
2024-05-24 00:08:47 -0600

    Reformat plugins/noice.lua

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -2,174 +2,180 @@
 -- It is highly customizable, allowing for a refined and user-friendly interface.
 
 return {
-        -- Plugin identifier and lazy loading configuration.
-        'folke/noice.nvim',
-        event = 'VeryLazy',
-        dependencies = {
-                "MunifTanjim/nui.nvim",
-                "rcarriga/nvim-notify",
+  -- Plugin identifier and lazy loading configuration.
+  'folke/noice.nvim',
+  event = 'VeryLazy',
+  dependencies = {
+    'MunifTanjim/nui.nvim',
+    'rcarriga/nvim-notify',
+  },
+  config = function()
+    require('noice').setup {
+      cmdline = {
+        enabled = true,
+        view = 'cmdline_popup',
+        opts = {
+          relative = 'editor',
+          position = '50%',
+          size = {
+            width = 60,
+            height = 'auto',
+          },
+          border = {
+            style = 'single',
+          },
         },
-        config = function()
-                require('noice').setup({
-                        cmdline = {
-                                enabled = true,
-                                view = "cmdline_popup",
-                                opts = {
-                                        relative = "editor",
-                                        position = "50%",
-                                        size = {
-                                                width = 60,
-                                                height = "auto",
-                                        },
-                                        border = {
-                                                style = "single"
-                                        },
-                                },
-                                format = {
-                                        cmdline = { pattern = "^:", icon = "", lang = "vim" },
-                                        search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
-                                        search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
-                                        filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
-                                        lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
-                                        help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
-                                        input = {},
-                                },
-                        },
-                        messages = {
-                                enabled = true,
-                                view = "notify",
-                                view_error = "notify",
-                                view_warn = "notify",
-                                view_history = "notify",
-                                view_search = "virtualtext",
-                        },
-                        popupmenu = {
-                                enabled = true,
-                                backend = "nui",
-                                kind_icons = {},
-                        },
-                        redirect = {
-                                view = "popup",
-                                filter = { event = "msg_show" },
-                        },
-                        commands = {
-                                history = {
-                                        view = "split",
-                                        opts = { enter = true, format = "details" },
-                                        filter = {
-                                                any = {
-                                                        { event = "notify" },
-                                                        { error = true },
-                                                        { warning = true },
-                                                        { event = "msg_show", kind = { "" } },
-                                                        { event = "lsp",      kind = "message" },
-                                                },
-                                        },
-                                },
-                                last = {
-                                        view = "popup",
-                                        opts = { enter = true, format = "details" },
-                                        filter = {
-                                                any = {
-                                                        { event = "notify" },
-                                                        { error = true },
-                                                        { warning = true },
-                                                        { event = "msg_show", kind = { "" } },
-                                                        { event = "lsp",      kind = "message" },
-                                                },
-                                        },
-                                        filter_opts = { count = 1 },
-                                },
-                                errors = {
-                                        view = "popup",
-                                        opts = { enter = true, format = "details" },
-                                        filter = { error = true },
-                                        filter_opts = { reverse = true },
-                                },
-                        },
-                        notify = {
-                                enabled = true,
-                                view = "notify",
-                        },
-                        lsp = {
-                                progress = {
-                                        enabled = true,
-                                        format = "lsp_progress",
-                                        format_done = "lsp_progress_done",
-                                        throttle = 1000 / 30,
-                                        view = "mini",
-                                },
-                                override = {
-                                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                                        ["vim.lsp.util.stylize_markdown"] = true,
-                                        ["cmp.entry.get_documentation"] = true,
-                                },
-                                hover = {
-                                        enabled = true,
-                                        silent = false,
-                                        view = nil,
-                                        opts = {},
-                                },
-                                signature = {
-                                        enabled = true,
-                                        auto_open = {
-                                                enabled = true,
-                                                trigger = true,
-                                                luasnip = true,
-                                                throttle = 50,
-                                        },
-                                        view = nil,
-                                        opts = {},
-                                },
-                                message = {
-                                        enabled = true,
-                                        view = "notify",
-                                        opts = {},
-                                },
-                                documentation = {
-                                        view = "hover",
-                                        opts = {
-                                                lang = "markdown",
-                                                replace = true,
-                                                render = "plain",
-                                                format = { "{message}" },
-                                                win_options = { concealcursor = "n", conceallevel = 3 },
-                                        },
-                                },
-                        },
-                        markdown = {
-                                hover = {
-                                        ["|(%S-)|"] = vim.cmd.help,
-                                        ["%[.-%]%((%S-)%)"] = require("noice.util").open,
-                                },
-                                highlights = {
-                                        ["|%S-|"] = "@text.reference",
-                                        ["@%S+"] = "@parameter",
-                                        ["^%s*(Parameters:)"] = "@text.title",
-                                        ["^%s*(Return:)"] = "@text.title",
-                                        ["^%s*(See also:)"] = "@text.title",
-                                        ["{%S-}"] = "@parameter",
-                                },
-                        },
-                        health = {
-                                checker = true,
-                        },
-                        smart_move = {
-                                enabled = true,
-                                excluded_filetypes = { "cmp_menu", "cmp_docs", "notify" },
-                        },
-                        presets = {
-                                bottom_search = false,
-                                command_palette = true,
-                                long_message_to_split = false,
-                                inc_rename = false,
-                                lsp_doc_border = false,
-                        },
-                        throttle = 1000 / 30,
-                        views = {},
-                        routes = {},
-                        status = {},
-                        format = {},
-                })
-        end,
+        format = {
+          cmdline = { pattern = '^:', icon = '', lang = 'vim' },
+          search_down = { kind = 'search', pattern = '^/', icon = ' ', lang = 'regex' },
+          search_up = { kind = 'search', pattern = '^%?', icon = ' ', lang = 'regex' },
+          filter = { pattern = '^:%s*!', icon = '$', lang = 'bash' },
+          lua = { pattern = { '^:%s*lua%s+', '^:%s*lua%s*=%s*', '^:%s*=%s*' }, icon = '', lang = 'lua' },
+          help = { pattern = '^:%s*he?l?p?%s+', icon = '' },
+          input = {},
+        },
+      },
+      messages = {
+        enabled = true,
+        view = 'notify',
+        view_error = 'notify',
+        view_warn = 'notify',
+        view_history = 'notify',
+        view_search = 'virtualtext',
+      },
+      popupmenu = {
+        enabled = true,
+        backend = 'nui',
+        kind_icons = {},
+      },
+      redirect = {
+        view = 'popup',
+        filter = { event = 'msg_show' },
+      },
+      commands = {
+        history = {
+          view = 'split',
+          opts = { enter = true, format = 'details' },
+          filter = {
+            any = {
+              { event = 'notify' },
+              { error = true },
+              { warning = true },
+              { event = 'msg_show', kind = { '' } },
+              { event = 'lsp', kind = 'message' },
+            },
+          },
+        },
+        last = {
+          view = 'popup',
+          opts = { enter = true, format = 'details' },
+          filter = {
+            any = {
+              { event = 'notify' },
+              { error = true },
+              { warning = true },
+              { event = 'msg_show', kind = { '' } },
+              { event = 'lsp', kind = 'message' },
+            },
+          },
+          filter_opts = { count = 1 },
+        },
+        errors = {
+          view = 'popup',
+          opts = { enter = true, format = 'details' },
+          filter = { error = true },
+          filter_opts = { reverse = true },
+        },
+      },
+      notify = {
+        enabled = true,
+        view = 'notify',
+      },
+      lsp = {
+        progress = {
+          enabled = true,
+          format = 'lsp_progress',
+          format_done = 'lsp_progress_done',
+          throttle = 1000 / 30,
+          view = 'mini',
+        },
+        override = {
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          ['vim.lsp.util.stylize_markdown'] = true,
+          ['cmp.entry.get_documentation'] = true,
+        },
+        hover = {
+          enabled = true,
+          silent = false,
+          view = nil,
+          opts = {},
+        },
+        signature = {
+          enabled = true,
+          auto_open = {
+            enabled = true,
+            trigger = true,
+            luasnip = true,
+            throttle = 50,
+          },
+          view = nil,
+          opts = {},
+        },
+        message = {
+          enabled = true,
+          view = 'notify',
+          opts = {},
+        },
+        documentation = {
+          view = 'hover',
+          opts = {
+            lang = 'markdown',
+            replace = true,
+            render = 'plain',
+            format = { '{message}' },
+            win_options = { concealcursor = 'n', conceallevel = 3 },
+          },
+        },
+      },
+      markdown = {
+        hover = {
+          ['|(%S-)|'] = vim.cmd.help,
+          ['%[.-%]%((%S-)%)'] = require('noice.util').open,
+        },
+        highlights = {
+          ['|%S-|'] = '@text.reference',
+          ['@%S+'] = '@parameter',
+          ['^%s*(Parameters:)'] = '@text.title',
+          ['^%s*(Return:)'] = '@text.title',
+          ['^%s*(See also:)'] = '@text.title',
+          ['{%S-}'] = '@parameter',
+        },
+      },
+      health = {
+        checker = true,
+      },
+      smart_move = {
+        enabled = true,
+        excluded_filetypes = { 'cmp_menu', 'cmp_docs', 'notify' },
+      },
+      presets = {
+        bottom_search = false,
+        command_palette = true,
+        long_message_to_split = false,
+        inc_rename = false,
+        lsp_doc_border = false,
+      },
+      throttle = 1000 / 30,
+      views = {
+        hover = {
+          border = {
+            style = 'rounded',
+          },
+        },
+      },
+      routes = {},
+      status = {},
+      format = {},
+    }
+  end,
 }
