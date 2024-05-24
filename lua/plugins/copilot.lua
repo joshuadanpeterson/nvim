be73b4312061e09015eaac48bc773a754ca4bfc7
Josh Peterson <joshuadanpeterson@gmail.com>
2024-05-24 00:19:09 -0600

    Updated formatting

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -5,37 +5,37 @@
 
 return {
   {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
     config = function()
-      require('copilot').setup({
+      require('copilot').setup {
         panel = {
           enabled = false,
           auto_refresh = false,
           keymap = {
-            jump_prev = "[[",
-            jump_next = "]]",
-            accept = "<CR>",
-            refresh = "gr",
-            open = "<M-CR>"
+            jump_prev = '[[',
+            jump_next = ']]',
+            accept = '<CR>',
+            refresh = 'gr',
+            open = '<M-CR>',
           },
           layout = {
-            position = "bottom", -- | top | left | right
-            ratio = 0.4
+            position = 'bottom', -- | top | left | right
+            ratio = 0.4,
           },
         },
         suggestion = {
           enabled = false,
-          auto_trigger = false,
+          auto_trigger = true,
           debounce = 75,
           keymap = {
-            accept = "<M-l>",
+            accept = '<M-l>',
             accept_word = false,
             accept_line = false,
-            next = "<M-]>",
-            prev = "<M-[>",
-            dismiss = "<C-]>",
+            next = '<M-]>',
+            prev = '<M-[>',
+            dismiss = '<C-]>',
           },
         },
         filetypes = {
@@ -47,13 +47,12 @@ return {
           hgcommit = false,
           svn = false,
           cvs = false,
-          ["."] = false,
+          ['.'] = false,
         },
         copilot_node_command = 'node', -- Node.js version must be > 18.x
         server_opts_overrides = {},
-      })
+      }
     end,
-  },
 
     -- require('copilot.panel').accept(),
     -- require('copilot.panel').jump_next(),
