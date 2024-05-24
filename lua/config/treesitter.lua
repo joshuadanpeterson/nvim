d38db716cc3d93f47d5f5353d686585acaa0e567
Josh Peterson <joshuadanpeterson@gmail.com>
2024-05-24 00:36:05 -0600

    Updated formatting

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -4,101 +4,133 @@
 -- Set this to true to skip the deprecated module loading for context_commentstring
 vim.g.skip_ts_context_commentstring_module = true
 
-require('plugins.treesitter')
+require 'plugins.treesitter'
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
 -- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
 vim.defer_fn(function()
-    require('nvim-treesitter.configs').setup {
-        -- Add languages to be installed here that you want installed for treesitter
-        ensure_installed = { "bash", "comment", "csv", "dockerfile", "dart", "go", "graphql", "http", "htmldjango", "json", "jq", "markdown", "php", "regex", "scss", "toml", "xml", "yaml", 'c', 'lua', 'vimdoc', 'vim', "python", "html", "css", "javascript", "jsx", "typescript", "tsx" },
+  require('nvim-treesitter.configs').setup {
+    -- Add languages to be installed here that you want installed for treesitter
+    ensure_installed = {
+      'bash',
+      'comment',
+      'csv',
+      'dockerfile',
+      'dart',
+      'go',
+      'graphql',
+      'http',
+      'htmldjango',
+      'json',
+      'jq',
+      'markdown',
+      'php',
+      'regex',
+      'scss',
+      'toml',
+      'xml',
+      'yaml',
+      'c',
+      'lua',
+      'vimdoc',
+      'vim',
+      'python',
+      'html',
+      'css',
+      'javascript',
+      'jsx',
+      'typescript',
+      'tsx',
+    },
 
-        -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-        auto_install = true,
-        sync_install = true,
+    -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+    auto_install = true,
+    sync_install = true,
 
-        highlight = { enable = true },
-        autotag = { enable = true },
-        playground = {
-            enable = true,
-            disable = {},
-            updatetime = 25,         -- Debounced time for highlighting nodes from source code
-            persist_queries = false, -- Persist queries across sessions
+    additional_vim_regex_highlighting = false,
+
+    highlight = { enable = true },
+    autotag = { enable = true },
+    playground = {
+      enable = true,
+      disable = {},
+      updatetime = 25, -- Debounced time for highlighting nodes from source code
+      persist_queries = false, -- Persist queries across sessions
+    },
+    rainbow = {
+      enable = true,
+      extended_mode = true, -- Also highlight other brackets/parentheses
+    },
+    -- Which query to use for finding delimiters
+    query = 'rainbow-parens',
+    -- Highlight the entire buffer all at once
+    strategy = require('ts-rainbow').strategy.global,
+    indent = { enable = true },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = '<A-space>',
+        node_incremental = '<A-space>',
+        scope_incremental = '<A-s>',
+        node_decremental = '<M-space>',
+      },
+    },
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+        keymaps = {
+          -- You can use the capture groups defined in textobjects.scm
+          ['<A-o>'] = '@parameter.outer',
+          ['<A-O>'] = '@parameter.inner',
+          ['<A-f>'] = '@function.outer',
+          ['<A-F>'] = '@function.inner',
+          ['<A-c>'] = '@class.outer',
+          ['<A-C>'] = '@class.inner',
         },
-        rainbow = {
-            enable = true,
-            extended_mode = true, -- Also highlight other brackets/parentheses
+      },
+      move = {
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+        goto_next_start = {
+          [']m'] = '@function.outer',
+          [']]'] = '@class.outer',
         },
-        -- Which query to use for finding delimiters
-        query = 'rainbow-parens',
-        -- Highlight the entire buffer all at once
-        strategy = require('ts-rainbow').strategy.global,
-        indent = { enable = true },
-        incremental_selection = {
-            enable = true,
-            keymaps = {
-                init_selection = '<A-space>',
-                node_incremental = '<A-space>',
-                scope_incremental = '<A-s>',
-                node_decremental = '<M-space>',
-            },
+        goto_next_end = {
+          [']M'] = '@function.outer',
+          [']['] = '@class.outer',
         },
-        textobjects = {
-            select = {
-                enable = true,
-                lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-                keymaps = {
-                    -- You can use the capture groups defined in textobjects.scm
-                    ['<A-o>'] = '@parameter.outer',
-                    ['<A-O>'] = '@parameter.inner',
-                    ['<A-f>'] = '@function.outer',
-                    ['<A-F>'] = '@function.inner',
-                    ['<A-c>'] = '@class.outer',
-                    ['<A-C>'] = '@class.inner',
-                },
-            },
-            move = {
-                enable = true,
-                set_jumps = true, -- whether to set jumps in the jumplist
-                goto_next_start = {
-                    [']m'] = '@function.outer',
-                    [']]'] = '@class.outer',
-                },
-                goto_next_end = {
-                    [']M'] = '@function.outer',
-                    [']['] = '@class.outer',
-                },
-                goto_previous_start = {
-                    ['[m'] = '@function.outer',
-                    ['[['] = '@class.outer',
-                },
-                goto_previous_end = {
-                    ['[M'] = '@function.outer',
-                    ['[]'] = '@class.outer',
-                },
-            },
-            swap = {
-                enable = true,
-                swap_next = {
-                    ['<leader>a'] = '@parameter.inner',
-                },
-                swap_previous = {
-                    ['<leader>A'] = '@parameter.inner',
-                },
-            },
-            -- Enable context_commentstring
-            context_commentstring = {
-                enable = true,
-                enable_autocmd = false,
-            },
+        goto_previous_start = {
+          ['[m'] = '@function.outer',
+          ['[['] = '@class.outer',
         },
-    }
+        goto_previous_end = {
+          ['[M'] = '@function.outer',
+          ['[]'] = '@class.outer',
+        },
+      },
+      swap = {
+        enable = true,
+        swap_next = {
+          ['<leader>a'] = '@parameter.inner',
+        },
+        swap_previous = {
+          ['<leader>A'] = '@parameter.inner',
+        },
+      },
+      -- Enable context_commentstring
+      context_commentstring = {
+        enable = true,
+        enable_autocmd = false,
+      },
+    },
+  }
 end, 0)
 
 -- Setup ts_context_commentstring
 require('ts_context_commentstring').setup {}
 
 -- Get Treesitter to support embeded languages
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.tsx.used_by = { "javascript", "javascriptreact", "typescript", "typescriptreact", "html" }
+local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+parser_config.tsx.used_by = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'html' }
