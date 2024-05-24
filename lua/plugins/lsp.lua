c4b9415f422ffa3d8c7f2a3f79698292d6c6dacd
Josh Peterson <joshuadanpeterson@gmail.com>
2024-05-24 00:11:23 -0600

    Updated Trouble to V3
    Changed to Beta vesion, added keymaps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -34,12 +34,58 @@ return {
   -- Trouble.nvim for an enhanced diagnostic list
   {
     'folke/trouble.nvim',
-    requires = 'kyazdani42/nvim-web-devicons',
-    config = function()
-      require('trouble').setup {
-        -- Configuration options
-      }
-    end,
+    dependencies = 'kyazdani42/nvim-web-devicons',
+    branch = 'dev', -- IMPORTANT! V3 Beta
+
+    keys = {
+      {
+        '<leader>xx',
+        '<cmd>Trouble diagnostics toggle<cr>',
+        desc = 'Diagnostics (Trouble)',
+      },
+      {
+        '<leader>xX',
+        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+        desc = 'Buffer Diagnostics (Trouble)',
+      },
+      {
+        '<leader>cs',
+        '<cmd>Trouble symbols toggle focus=false<cr>',
+        desc = 'Symbols (Trouble)',
+      },
+      {
+        '<leader>cl',
+        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+        desc = 'LSP Definitions / references / ... (Trouble)',
+      },
+      {
+        '<leader>xL',
+        '<cmd>Trouble loclist toggle<cr>',
+        desc = 'Location List (Trouble)',
+      },
+      {
+        '<leader>xQ',
+        '<cmd>Trouble qflist toggle<cr>',
+        desc = 'Quickfix List (Trouble)',
+      },
+    },
+    opts = {
+      modes = {
+        preview_float = {
+          mode = 'diagnostics',
+          preview = {
+            type = 'float',
+            relative = 'editor',
+            border = 'rounded',
+            title = 'Preview',
+            title_pos = 'center',
+            position = { 0, -2 },
+            size = { width = 0.3, height = 0.3 },
+            zindex = 200,
+          },
+        },
+      },
+    }, -- for default options, refer to the configuration section for custom setup.
   },
 
   -- nvim-emmet | for integration with emmet-language-server
