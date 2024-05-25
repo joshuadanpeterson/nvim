227ea0c6e75685ce4ebb40d78cb716d4e436cacb
Josh Peterson <joshuadanpeterson@gmail.com>
2024-05-25 02:20:12 -0600

    style: Update visual settings in visuals.lua

--- lua/plugins/visuals.lua
+++ lua/plugins/visuals.lua
@@ -96,11 +96,11 @@ return {
               -- showtabline = 1,
               signcolumn = 'no',
               statusline = '',
-              cmdheight = 1,
+              -- cmdheight = 1,
               laststatus = 0,
               showcmd = false,
-              showmode = false,
-              ruler = false,
+              -- showmode = false,
+              -- ruler = false,
               numberwidth = 1,
             },
             callbacks = { -- run functions when opening/closing Minimalist mode
@@ -151,13 +151,13 @@ return {
     'folke/zen-mode.nvim',
     opts = {
       window = {
-        backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
+        backdrop = 0.10, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
         -- height and width can be:
         -- * an absolute number of cells when > 1
         -- * a percentage of the width / height of the editor when <= 1
         -- * a function that returns the width or the height
         width = 120, -- width of the Zen window
-        height = 1, -- height of the Zen window
+        height = 0.90, -- height of the Zen window
         -- by default, no options are changed for the Zen window
         -- uncomment any of the options below, or add other vim.wo options you want to apply
         options = {
