194e820e036491366b574c2db56e50de784a867d
Josh Peterson <joshuadanpeterson@gmail.com>
2024-06-07 16:28:29 -0600

    refactor(plugins/finder.lua): Added lazy loading capabilities to finder plugins

--- lua/plugins/finder.lua
+++ lua/plugins/finder.lua
@@ -4,113 +4,124 @@
 	custom.plugins.finder: Sets up file and content search tools within Neovim, such as Telescope or FZF. This includes configuring search behaviors, customizing the user interface, and defining keybindings for launching searches.
 ]]
 
-
 return {
 
-	-- Telescope
-	-- Sets vim.ui.select to telescope. That means for example that neovim core stuff can fill the telescope picker
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-	},
+  -- Telescope UI Select
+  {
+    'nvim-telescope/telescope-ui-select.nvim',
+    event = 'VimEnter',
+  },
 
-	-- Fuzzy Finder Algorithm, requires local dependencies to be built. Only loads if make is available.
-	{
-		'nvim-telescope/telescope-fzf-native.nvim',
-	},
+  -- Fuzzy Finder Algorithm
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build = 'make',
+    cond = vim.fn.executable 'make' == 1,
+    event = 'VimEnter',
+  },
 
-	-- telescope-project: manage and switch between projects with telescope.
-	{
-		'nvim-telescope/telescope-project.nvim',
-	},
+  -- Telescope Project
+  {
+    'nvim-telescope/telescope-project.nvim',
+    cmd = 'Telescope',
+  },
 
-	-- fzy native search
-	{
-		'nvim-telescope/telescope-fzy-native.nvim',
-	},
+  -- Fzy Native Search
+  {
+    'nvim-telescope/telescope-fzy-native.nvim',
+    cmd = 'Telescope',
+  },
 
-	-- telescope-zoxide
-	{
-		'jvgrootveld/telescope-zoxide',
-	},
+  -- Telescope Zoxide
+  {
+    'jvgrootveld/telescope-zoxide',
+    cmd = 'Telescope',
+  },
 
-	-- telescope-helpgrep.nvim
-	{
-		'catgoose/telescope-helpgrep.nvim',
-	},
+  -- Telescope Helpgrep
+  {
+    'catgoose/telescope-helpgrep.nvim',
+    cmd = 'Telescope',
+  },
 
-	-- telescope-repo: search filesystem for git repos
-	{
-		'cljoly/telescope-repo.nvim',
-	},
+  -- Telescope Repo
+  {
+    'cljoly/telescope-repo.nvim',
+    cmd = 'Telescope',
+  },
 
-	-- fzf vim: integrates the fzf command-line fuzzy finder with vim.
-	{
-		'junegunn/fzf.vim',
-		{
-			"nvim-telescope/telescope.nvim",
-			dependencies = { "nvim-lua/plenary.nvim" },
-		},
-	},
+  -- Fzf Vim
+  {
+    'junegunn/fzf.vim',
+    cmd = 'Fzf',
+    dependencies = {
+      {
+        'nvim-telescope/telescope.nvim',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        cmd = 'Telescope',
+      },
+    },
+  },
 
-	-- Telescope GPT: adds ChatGPT functionality to Telescope
-	{
-		'HPRIOR/telescope-gpt',
-		dependencies = {
-			'nvim-telescope/telescope.nvim',
-			'jackMort/ChatGPT.nvim'
-		},
-	},
+  -- Telescope GPT
+  {
+    'HPRIOR/telescope-gpt',
+    dependencies = {
+      'nvim-telescope/telescope.nvim',
+      'jackMort/ChatGPT.nvim',
+    },
+    cmd = 'Telescope',
+  },
 
-	-- tldr Telescope extension
-	{
-		'mrjones2014/tldr.nvim',
-		dependencies = { 'nvim-telescope/telescope.nvim' },
-	},
+  -- Tldr Telescope Extension
+  {
+    'mrjones2014/tldr.nvim',
+    dependencies = { 'nvim-telescope/telescope.nvim' },
+    cmd = 'Telescope',
+  },
 
-	-- Fuzzy Finder (files, lsp, etc) with dependencies including plenary.nvim and telescope-ui-select.nvim
-	{
-		'nvim-telescope/telescope.nvim',
-		branch = '0.1.x',
-		dependencies = {
-			'nvim-lua/plenary.nvim',
-			{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make', cond = vim.fn.executable 'make' == 1 },
-			'nvim-telescope/telescope-ui-select.nvim',
-			'andrew-george/telescope-themes',
-			'Lilja/telescope-swap-files',
-			'tsakirist/telescope-lazy.nvim',
-			'Myzel394/jsonfly.nvim',
-		},
-	},
+  -- Fuzzy Finder (files, lsp, etc)
+  {
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make', cond = vim.fn.executable 'make' == 1 },
+      'nvim-telescope/telescope-ui-select.nvim',
+      'andrew-george/telescope-themes',
+      'Lilja/telescope-swap-files',
+      'tsakirist/telescope-lazy.nvim',
+      'Myzel394/jsonfly.nvim',
+    },
+    cmd = 'Telescope',
+  },
 
-	-- Tmux Telescope Plugin
-	{
-		"shivamashtikar/tmuxjump.vim",
-		dependencies = {
-			"junegunn/fzf.vim", -- fzf.vim for fuzzy finding
-			"nvim-telescope/telescope.nvim", -- telescope.nvim for enhanced searching
-		},
-		config = function()
-			-- Optionally set tmuxjump to use Telescope
-			vim.g.tmuxjump_telescope = true
+  -- Tmux Telescope Plugin
+  {
+    'shivamashtikar/tmuxjump.vim',
+    dependencies = {
+      'junegunn/fzf.vim',
+      'nvim-telescope/telescope.nvim',
+    },
+    config = function()
+      vim.g.tmuxjump_telescope = true
+      vim.cmd [[
+        autocmd FileType purescript nnoremap <leader>ft :TmuxJumpFile purs<CR>
+        autocmd FileType purescript nnoremap <leader>; :TmuxJumpFirst purs<CR>
+      ]]
+    end,
+  },
 
-			-- For specific file types, like purescript
-			vim.cmd([[
-			autocmd FileType purescript nnoremap <leader>ft :TmuxJumpFile purs<CR>
-			autocmd FileType purescript nnoremap <leader>; :TmuxJumpFirst purs<CR>
-		    ]])
-		end
-	},
+  -- Ranger Neovim Plugin
+  {
+    'kevinhwang91/rnvimr',
+    cmd = 'RnvimrToggle',
+  },
 
-	-- Ranger Neovim plugin
-	{
-		'kevinhwang91/rnvimr',
-	},
-
-	-- Flash.nvim
-	{
-		"folke/flash.nvim",
-		event = "VeryLazy",
-		---@type Flash.Config
-		opts = {},
-	},
+  -- Flash.nvim
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    opts = {},
+  },
 }
