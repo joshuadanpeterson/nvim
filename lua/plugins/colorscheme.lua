91d190213f71e4845de47e2b6c9a82833c81a722
Josh Peterson <joshuadanpeterson@gmail.com>
2024-06-07 16:27:16 -0600

    refactor(plugins/colorscheme.lua): Added lazy loading capabilities to colorscheme plugins

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,62 +2,41 @@
 --[[
         custom.plugins.colorscheme: Configures the visual theme of Neovim, including colors for syntax highlighting, background, and other UI elements. It might specify a particular colorscheme plugin and any customizations to it.
 ]]
+
 return {
-
-  -- nord theme
-  -- Transparent Nord Theme
+  -- Sonokai theme
   {
-    'shaunsingh/nord.nvim',
-    config = function()
-      -- load the theme
-      vim.cmd 'colorscheme nord'
-      require('nord').set {
-        transparent = true,
-        -- styles = {
-        --   sidebars = 'transparent',
-        --   floats = 'transparent',
-        -- },
-      }
-    end,
-  },
-
-  -- Transparent Background
-  {
-    'tribela/vim-transparent',
-  },
-
-  -- NightFox theme
-  -- A highly customizable theme for vim and neovim with support for lsp, treesitter and a variety of plugins.
-  {
-    'EdenEast/nightfox.nvim',
-  },
-
-  -- Nordic theme
-  -- Darker Nord theme
-  {
-    'AlexvZyl/nordic.nvim',
+    'sainnhe/sonokai',
     lazy = false,
-    priority = 1000,
+    priority = 1000, -- Ensure it loads first
     config = function()
-      require('nordic').load()
-      -- vim.cmd 'colorscheme nordic'
-    end,
-  },
+      -- You can set some Sonokai specific options here if needed
+      vim.g.sonokai_style = 'andromeda' -- or other styles: 'default', 'andromeda', 'atlantis', 'shusia', 'maia'
+      vim.g.sonokai_enable_italic = 1
+      vim.g.sonokai_disable_italic_comment = 0
+      vim.g.sonokai_cursor = 'auto'
+      vim.g.sonokai_transparent_background = 0
+      vim.g.sonokai_show_eob = 1
+      vim.g.sonokai_diagnostic_text_highlight = 1
+      vim.g.sonokai_diagnostic_line_highlight = 1
+      vim.g.sonokai_diagnostic_virtual_text = 'colored'
 
-  -- Material theme
-  -- A port of the Material color scheme for Neovim
-  {
-    'marko-cerovac/material.nvim',
-    -- config = function()
-    --   require('material').setup {
-    --     vim.cmd 'colorscheme material',
-    --   }
-    -- end,
+      -- Set the colorscheme
+      vim.cmd 'colorscheme sonokai'
+
+      -- Set custom highlights for autocomplete menu after colorscheme is loaded
+      vim.cmd [[
+        highlight Pmenu guibg=#D3D3D3
+        highlight PmenuSel guibg=#C0C0C0 guifg=NONE
+        highlight CursorLine guibg=#D3D3D3
+      ]]
+    end,
   },
 
   -- GitHub theme
   {
     'projekt0n/github-nvim-theme',
+    lazy = true,
     config = function()
       require('github-theme').setup {
         darken = {
@@ -66,8 +45,45 @@ return {
         modules = {
           'which-key',
         },
-        vim.cmd 'colorscheme github_dark_dimmed',
       }
     end,
+  },
+
+  -- Nord theme
+  {
+    'shaunsingh/nord.nvim',
+    event = 'VimEnter',
+    config = function()
+      require('nord').set {
+        transparent = true,
+      }
+    end,
+  },
+
+  -- Transparent Background
+  {
+    'tribela/vim-transparent',
+    event = 'VimEnter',
+  },
+
+  -- NightFox theme
+  {
+    'EdenEast/nightfox.nvim',
+    event = 'VimEnter',
+  },
+
+  -- Nordic theme
+  {
+    'AlexvZyl/nordic.nvim',
+    event = 'VimEnter',
+    config = function()
+      require('nordic').load()
+    end,
+  },
+
+  -- Material theme
+  {
+    'marko-cerovac/material.nvim',
+    event = 'VimEnter',
   },
 }
