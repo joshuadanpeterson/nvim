49681bda2a5115f98f27990656523b04d7ffb1f9
Josh Peterson <joshuadanpeterson@gmail.com>
2024-06-07 16:31:27 -0600

    refactor(plugins/ui.lua): Added lazy loading capabilities to ui plugins

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -4,44 +4,42 @@
 ]]
 
 return {
-	-- nvim-web-devicons: adds filetype icons to neovim plugins.
-	{
-		'kyazdani42/nvim-web-devicons',
-		lazy = true
-	},
+  -- nvim-web-devicons: adds filetype icons to neovim plugins
+  {
+    'kyazdani42/nvim-web-devicons',
+    event = 'BufReadPre',
+  },
 
-	-- nvim-nonicons: more icons
-	{
-		'yamatsum/nvim-nonicons',
-		requires = { 'kyazdani42/nvim-web-devicons' },
-		config = function()
-			require('nvim-nonicons').setup {}
-		end,
-	},
+  -- nvim-nonicons: more icons
+  {
+    'yamatsum/nvim-nonicons',
+    dependencies = { 'kyazdani42/nvim-web-devicons' },
+    event = 'BufReadPre',
+    config = function()
+      require('nvim-nonicons').setup {}
+    end,
+  },
 
-	-- -- vim-startify: provides a startup screen with session management.
-	-- {
-	-- 	'mhinz/vim-startify'
-	-- },
+  -- Standalone UI for Neovim LSP progress
+  {
+    'j-hui/fidget.nvim',
+    tag = 'v1.0.0',
+    event = 'BufReadPre',
+  },
 
-	-- Standalone UI for Neovim LSP progress.
-	{
-		'j-hui/fidget.nvim',
-		tag = "v1.0.0",
-	},
+  -- dressing.nvim - improves UI
+  {
+    'stevearc/dressing.nvim',
+    event = 'BufReadPre',
+    opts = {},
+  },
 
-	-- dressing.nvim - improves UI
-	{
-		'stevearc/dressing.nvim',
-		opts = {},
-	},
-
-	-- Customizable Neovim dashboard for better startup experience.
-	{
-		"nvimdev/dashboard-nvim",
-		event = "VimEnter",
-		opts = function()
-			local logo = [[
+  -- Customizable Neovim dashboard for better startup experience
+  {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    opts = function()
+      local logo = [[
              ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
              ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z
              ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z
@@ -50,84 +48,78 @@ return {
              ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝
         ]]
 
-			logo = string.rep("\n", 8) .. logo .. "\n\n"
+      logo = string.rep('\n', 8) .. logo .. '\n\n'
 
-			local opts = {
-				theme = "doom",
-				hide = {
-					-- this is taken care of by lualine
-					-- enabling this messes up the actual laststatus setting after loading a file
-					statusline = false,
-				},
-				config = {
-					header = vim.split(logo, "\n"),
-					-- stylua: ignore
-					center = {
-						{ action = "Telescope find_files", desc = " Find file", icon = " ", key = "f" },
-						{ action = "ene | startinsert", desc = " New file", icon = " ", key = "n" },
-						{ action = "Telescope oldfiles", desc = " Recent files", icon = " ", key = "r" },
-						{ action = "Telescope live_grep", desc = " Find text", icon = " ", key = "g" },
-						{ action = [[lua require("lazyvim.util").telescope.config_files()()]], desc = " Config", icon = " ", key = "c" },
-						{ action = 'lua require("persistence").load()', desc = " Restore Session", icon = " ", key = "s" },
-						{ action = "LazyExtras", desc = " Lazy Extras", icon = " ", key = "x" },
-						{ action = "Lazy", desc = " Lazy", icon = "󰒲 ", key = "l" },
-						{ action = "Leet", desc = " LeetCode", icon = " ", key = "d" },
-						{ action = "qa", desc = " Quit", icon = " ", key = "q" },
-					},
-					footer = function()
-						local stats = require("lazy").stats()
-						local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-						return { "⚡ Neovim loaded " ..
-						stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
-					end,
-				},
-			}
+      local opts = {
+        theme = 'doom',
+        hide = {
+          statusline = false,
+        },
+        config = {
+          header = vim.split(logo, '\n'),
+          center = {
+            { action = 'Telescope find_files', desc = ' Find file', icon = ' ', key = 'f' },
+            { action = 'ene | startinsert', desc = ' New file', icon = ' ', key = 'n' },
+            { action = 'Telescope oldfiles', desc = ' Recent files', icon = ' ', key = 'r' },
+            { action = 'Telescope live_grep', desc = ' Find text', icon = ' ', key = 'g' },
+            { action = [[lua require("lazyvim.util").telescope.config_files()()]], desc = ' Config', icon = ' ', key = 'c' },
+            { action = 'lua require("persistence").load()', desc = ' Restore Session', icon = ' ', key = 's' },
+            { action = 'LazyExtras', desc = ' Lazy Extras', icon = ' ', key = 'x' },
+            { action = 'Lazy', desc = ' Lazy', icon = '󰒲 ', key = 'l' },
+            { action = 'Leet', desc = ' LeetCode', icon = ' ', key = 'd' },
+            { action = 'qa', desc = ' Quit', icon = ' ', key = 'q' },
+          },
+          footer = function()
+            local stats = require('lazy').stats()
+            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+            return { '⚡ Neovim loaded ' .. stats.loaded .. '/' .. stats.count .. ' plugins in ' .. ms .. 'ms' }
+          end,
+        },
+      }
 
-			for _, button in ipairs(opts.config.center) do
-				button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
-				button.key_format = "  %s"
-			end
+      for _, button in ipairs(opts.config.center) do
+        button.desc = button.desc .. string.rep(' ', 43 - #button.desc)
+        button.key_format = '  %s'
+      end
 
-			-- close Lazy and re-open when the dashboard is ready
-			if vim.o.filetype == "lazy" then
-				vim.cmd.close()
-				vim.api.nvim_create_autocmd("User", {
-					pattern = "DashboardLoaded",
-					callback = function()
-						require("lazy").show()
-					end,
-				})
-			end
+      if vim.o.filetype == 'lazy' then
+        vim.cmd.close()
+        vim.api.nvim_create_autocmd('User', {
+          pattern = 'DashboardLoaded',
+          callback = function()
+            require('lazy').show()
+          end,
+        })
+      end
 
-			return opts
-		end,
-	},
+      return opts
+    end,
+  },
 
-	-- tmuxline
-	{
-		'edkolev/tmuxline.vim'
-	},
+  -- tmuxline
+  {
+    'edkolev/tmuxline.vim',
+    event = 'VimEnter',
+  },
 
-	-- emoji.nvim
-	{
-		"allaman/emoji.nvim",
-		version = "1.0.0", -- optionally pin to a tag
-		ft = "markdown", -- adjust to your needs
-		dependencies = {
-			-- optional for nvim-cmp integration
-			"hrsh7th/nvim-cmp",
-			-- optional for telescope integration
-			"nvim-telescope/telescope.nvim",
-		},
-		opts = {
-			-- default is false
-			enable_cmp_integration = true,
-		},
-	},
+  -- emoji.nvim
+  {
+    'allaman/emoji.nvim',
+    version = '1.0.0',
+    ft = 'markdown',
+    dependencies = {
+      'hrsh7th/nvim-cmp',
+      'nvim-telescope/telescope.nvim',
+    },
+    opts = {
+      enable_cmp_integration = true,
+    },
+  },
 
-	-- glow: markdown preview
-	{
-		{ "ellisonleao/glow.nvim", config = true, cmd = "Glow" },
-	},
-
+  -- glow: markdown preview
+  {
+    'ellisonleao/glow.nvim',
+    config = true,
+    cmd = 'Glow',
+  },
 }
