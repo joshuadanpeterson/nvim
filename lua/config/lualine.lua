2144821c171da2a0c468cd662ab3336d06bec8b1
Josh Peterson <joshuadanpeterson@gmail.com>
2024-06-14 17:24:21 -0600

    feat(config/lualine): Added lualine-lsp-progress

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -3,6 +3,19 @@
     Customizes the status line in Neovim using the Lualine plugin. Configurations typically involve setting the appearance of the status line, choosing which components to display, and possibly integrating with other plugins for status information.
 ]]
 
+-- Color for highlights
+local colors = {
+  yellow = '#ECBE7B',
+  cyan = '#008080',
+  darkblue = '#081633',
+  green = '#98be65',
+  orange = '#FF8800',
+  violet = '#a9a1e1',
+  magenta = '#c678dd',
+  blue = '#51afef',
+  red = '#ec5f67',
+}
+
 -- Setup nonicons
 local icons = require 'nvim-nonicons'
 local icon = icons.get 'git-branch'
@@ -117,3 +130,42 @@ local config = {
 
 -- Setup lualine with the defined configuration
 require('lualine').setup(config)
+
+-- lualine-lsp-progress config
+-- Inserts a component in lualine_c at left section
+local function ins_left(component)
+  table.insert(config.sections.lualine_c, component)
+end
+
+-- Inserts a component in lualine_x ot right section
+-- local function ins_right(component)
+--   table.insert(config.sections.lualine_x, component)
+-- end
+
+ins_left {
+  'lsp_progress',
+  display_components = { 'lsp_client_name', 'spinner', { 'title', 'percentage', 'message' } },
+  -- With spinner
+  -- display_components = { 'lsp_client_name', 'spinner', { 'title', 'percentage', 'message' }},
+  colors = {
+    percentage = colors.cyan,
+    title = colors.cyan,
+    message = colors.cyan,
+    spinner = colors.cyan,
+    lsp_client_name = colors.magenta,
+    use = true,
+  },
+  separators = {
+    component = ' ',
+    progress = ' | ',
+    message = { pre = '(', post = ')' },
+    percentage = { pre = '', post = '%% ' },
+    title = { pre = '', post = ': ' },
+    lsp_client_name = { pre = '[', post = ']' },
+    spinner = { pre = '', post = '' },
+    message = { commenced = 'In Progress', completed = 'Completed' },
+  },
+  display_components = { 'lsp_client_name', 'spinner', { 'title', 'percentage', 'message' } },
+  timer = { progress_enddelay = 500, spinner = 1000, lsp_client_name_enddelay = 1000 },
+  spinner_symbols = { '🌑 ', '🌒 ', '🌓 ', '🌔 ', '🌕 ', '🌖 ', '🌗 ', '🌘 ' },
+}
