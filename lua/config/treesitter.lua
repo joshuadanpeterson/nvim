cb9a151e59fbffac2022e1f3079d8ccbc3c7927e
Josh Peterson <joshuadanpeterson@gmail.com>
2024-06-20 20:01:29 -0600

    refactor(config/treesitter): Updated keymaps of textobjects

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -70,10 +70,10 @@ vim.defer_fn(function()
     incremental_selection = {
       enable = true,
       keymaps = {
-        init_selection = '<A-space>',
-        node_incremental = '<A-space>',
-        scope_incremental = '<A-s>',
-        node_decremental = '<M-space>',
+        init_selection = '<C-space>',
+        node_incremental = '<C-space>',
+        scope_incremental = false,
+        node_decremental = '<C-bs>',
       },
     },
     textobjects = {
@@ -82,47 +82,77 @@ vim.defer_fn(function()
         lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
         keymaps = {
           -- You can use the capture groups defined in textobjects.scm
-          ['<A-o>'] = '@parameter.outer',
-          ['<A-O>'] = '@parameter.inner',
-          ['<A-f>'] = '@function.outer',
-          ['<A-F>'] = '@function.inner',
-          ['<A-c>'] = '@class.outer',
-          ['<A-C>'] = '@class.inner',
+          ['a='] = { query = '@assignment.outer', desc = 'Select outer part of an assignment' },
+          ['i='] = { query = '@assignment.inner', desc = 'Select inner part of an assignment' },
+          ['l='] = { query = '@assignment.lhs', desc = 'Select left hand side of an assignment' },
+          ['r='] = { query = '@assignment.rhs', desc = 'Select right hand side of an assignment' },
+
+          ['aa'] = { query = '@parameter.outer', desc = 'Select outer part of a parameter/argument' },
+          ['ia'] = { query = '@parameter.inner', desc = 'Select inner part of a parameter/argument' },
+
+          ['ai'] = { query = '@conditional.outer', desc = 'Select outer part of a conditional' },
+          ['ii'] = { query = '@conditional.inner', desc = 'Select inner part of a conditional' },
+
+          ['al'] = { query = '@loop.outer', desc = 'Select outer part of a loop' },
+          ['il'] = { query = '@loop.inner', desc = 'Select inner part of a loop' },
+
+          ['af'] = { query = '@call.outer', desc = 'Select outer part of a function call' },
+          ['if'] = { query = '@call.inner', desc = 'Select inner part of a function call' },
+
+          ['am'] = { query = '@function.outer', desc = 'Select outer part of a method/function definition' },
+          ['im'] = { query = '@function.inner', desc = 'Select inner part of a method/function definition' },
+
+          ['ac'] = { query = '@class.outer', desc = 'Select outer part of a class' },
+          ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class' },
+        },
+      },
+      swap = {
+        enable = true,
+        swap_next = {
+          ['<leader>Sa'] = { '@parameter.inner', desc = 'Swap parameters/argument with next' },
+          ['<leader>Sf'] = { '@function.outer', desc = 'Swap function with next' },
+        },
+        swap_previous = {
+          ['<leader>SA'] = { '@parameter.inner', desc = 'Swap parameters/argument with prev' },
+          ['<leader>SF'] = { '@function.outer', desc = 'Swap function with previous' },
         },
       },
       move = {
         enable = true,
         set_jumps = true, -- whether to set jumps in the jumplist
         goto_next_start = {
-          [']m'] = '@function.outer',
-          [']]'] = '@class.outer',
+          [']f'] = { query = '@call.outer', desc = 'Next function call start' },
+          [']m'] = { query = '@function.outer', desc = 'Next method/function def start' },
+          [']c'] = { query = '@class.outer', desc = 'Next class start' },
+          [']i'] = { query = '@conditional.outer', desc = 'Next conditional start' },
+          [']l'] = { query = '@loop.outer', desc = 'Next loop start' },
+
+          -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+          -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+          [']s'] = { query = '@scope', query_group = 'locals', desc = 'Next scope' },
+          [']z'] = { query = '@fold', query_group = 'folds', desc = 'Next fold' },
         },
         goto_next_end = {
-          [']M'] = '@function.outer',
-          [']['] = '@class.outer',
+          [']F'] = { query = '@call.outer', desc = 'Next function call end' },
+          [']M'] = { query = '@function.outer', desc = 'Next method/function def end' },
+          [']C'] = { query = '@class.outer', desc = 'Next class end' },
+          [']I'] = { query = '@conditional.outer', desc = 'Next conditional end' },
+          [']L'] = { query = '@loop.outer', desc = 'Next loop end' },
         },
         goto_previous_start = {
-          ['[m'] = '@function.outer',
-          ['[['] = '@class.outer',
+          ['[f'] = { query = '@call.outer', desc = 'Prev function call start' },
+          ['[m'] = { query = '@function.outer', desc = 'Prev method/function def start' },
+          ['[c'] = { query = '@class.outer', desc = 'Prev class start' },
+          ['[i'] = { query = '@conditional.outer', desc = 'Prev conditional start' },
+          ['[l'] = { query = '@loop.outer', desc = 'Prev loop start' },
         },
         goto_previous_end = {
-          ['[M'] = '@function.outer',
-          ['[]'] = '@class.outer',
+          ['[F'] = { query = '@call.outer', desc = 'Prev function call end' },
+          ['[M'] = { query = '@function.outer', desc = 'Prev method/function def end' },
+          ['[C'] = { query = '@class.outer', desc = 'Prev class end' },
+          ['[I'] = { query = '@conditional.outer', desc = 'Prev conditional end' },
+          ['[L'] = { query = '@loop.outer', desc = 'Prev loop end' },
         },
-      },
-      swap = {
-        enable = true,
-        swap_next = {
-          ['<leader>a'] = '@parameter.inner',
-        },
-        swap_previous = {
-          ['<leader>A'] = '@parameter.inner',
-        },
-      },
-      -- Enable context_commentstring
-      context_commentstring = {
-        enable = true,
-        enable_autocmd = false,
       },
     },
   }
