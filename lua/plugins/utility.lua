ed5feb7c33e93b8e471dbffa53e4f2ac10156ebb
Josh Peterson <joshuadanpeterson@gmail.com>
2024-06-20 20:09:09 -0600

    feat(plugins/utility): Added tmux related plugins
    Added tmux.nvim and updated config for vim-tpipeline to merge statuslines

--- lua/plugins/utility.lua
+++ lua/plugins/utility.lua
@@ -254,11 +254,37 @@ return {
     },
   },
 
+  -- tmux.nvim
+  {
+    'aserowy/tmux.nvim',
+    event = 'VimEnter',
+    config = function()
+      require('tmux').setup {
+        copy_sync = {
+          enable = false,
+        },
+      }
+    end,
+  },
+
   -- vim-tpipeline
   {
     'vimpostor/vim-tpipeline',
+    event = 'VeryLazy',
+    init = function()
+      vim.g.tpipeline_autoembed = 0
+      vim.g.tpipeline_statusline = ''
+    end,
+    config = function()
+      vim.cmd.hi { 'link', 'StatusLine', 'WinSeparator' }
+      vim.g.tpipeline_statusline = ''
+      vim.o.laststatus = 0
+      vim.o.fillchars = 'stl:─,stlnc:─'
+    end,
+    cond = function()
+      return vim.env.TMUX ~= nil
+    end,
   },
-
   -- firenvim for using Neovim in Chrome
   {
     'glacambre/firenvim',
