3695915e968bd2070f0e53d35f8ffa366753b96c
Josh Peterson <joshuadanpeterson@gmail.com>
2024-07-11 23:39:20 -0600

    refactor(plugins/visuals): Change cursor plugins
    - Comment out auto-cursorline
    - Add modes.nvim
    - Changes color of highlights based on mode

--- lua/plugins/visuals.lua
+++ lua/plugins/visuals.lua
@@ -286,13 +286,48 @@ return {
   },
 
   -- auto-cursorline.nvim: For a highlighted cursorline
+  -- {
+  --   'delphinus/auto-cursorline.nvim',
+  --   config = function()
+  --     require('auto-cursorline').setup {}
+  --   end,
+  -- },
+
+  -- mode: cursor highlighting/coloring
   {
-    'delphinus/auto-cursorline.nvim',
+    'mvllow/modes.nvim',
+    tag = 'v0.2.0',
     config = function()
-      require('auto-cursorline').setup {}
+      require('modes').setup {
+        colors = {
+          bg = '', -- Optional bg param, defaults to Normal hl group
+          copy = '#f5c359',
+          delete = '#c75c6a',
+          insert = '#78ccc5',
+          visual = '#9745be',
+        },
+
+        -- Set opacity for cursorline and number background
+        line_opacity = 0.15,
+
+        -- Enable cursor highlights
+        set_cursor = true,
+
+        -- Enable cursorline initially, and disable cursorline for inactive windows
+        -- or ignored filetypes
+        set_cursorline = true,
+
+        -- Enable line number highlights to match cursorline
+        set_number = true,
+
+        -- Disable modes highlights in specified filetypes
+        -- Please PR commonly ignored filetypes
+        ignore_filetypes = { 'NvimTree', 'TelescopePrompt' },
+      }
     end,
   },
 
+  -- mini.icons
   -- typewriter.vim: Typewriter sounds for Vim
   -- Currently working on developing PR for Neovim adaptation
   {
