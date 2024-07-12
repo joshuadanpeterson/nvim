654da239ef0d422cc66d44809cae4450b968f0a7
Josh Peterson <joshuadanpeterson@gmail.com>
2024-07-11 23:32:36 -0600

    refactor(plugins/which-key): Update window config
    - Change margin and padding settings

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -14,7 +14,7 @@ return {
     -- Define plugin options
     local opts = {
       plugins = {
-        marks = true, -- shows a list of your marks on ' and `
+        marks = true,     -- shows a list of your marks on ' and `
         registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
         -- Add other plugin configurations here...
         spelling = {
@@ -52,27 +52,27 @@ return {
       },
       popup_mappings = {
         scroll_down = '<c-d>', -- binding to scroll down inside the popup
-        scroll_up = '<c-u>', -- binding to scroll up inside the popup
+        scroll_up = '<c-u>',   -- binding to scroll up inside the popup
       },
       window = {
-        border = 'single', -- none, single, double, shadow
-        position = 'bottom', -- bottom, top
-        margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]. When between 0 and 1, will be treated as a percentage of the screen size.
-        padding = { 1, 2, 1, 2 }, -- extra window padding [top, right, bottom, left]
-        winblend = 0, -- value between 0-100 0 for fully opaque and 100 for fully transparent
-        zindex = 1000, -- positive value to position WhichKey above other floating windows.
+        border = 'single',        -- none, single, double, shadow
+        position = 'bottom',      -- bottom, top
+        margin = { 1, 1, 1, 1 },  -- extra window margin [top, right, bottom, left]
+        padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+        winblend = 0,             -- value between 0-100 0 for fully opaque and 100 for fully transparent
+        zindex = 1000,            -- positive value to position WhichKey above other floating windows.
       },
       layout = {
-        height = { min = 4, max = 25 }, -- min and max height of the columns
-        width = { min = 20, max = 50 }, -- min and max width of the columns
-        spacing = 3, -- spacing between columns
-        align = 'left', -- align columns left, center or right
+        height = { min = 4, max = 25 },                                                 -- min and max height of the columns
+        width = { min = 20, max = 50 },                                                 -- min and max width of the columns
+        spacing = 3,                                                                    -- spacing between columns
+        align = 'left',                                                                 -- align columns left, center or right
       },
-      ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
+      ignore_missing = false,                                                           -- enable this to hide mappings for which you didn't specify a label
       hidden = { '<silent>', '<cmd>', '<Cmd>', '<CR>', '^:', '^ ', '^call ', '^lua ' }, -- hide mapping boilerplate
-      show_help = true, -- show a help message in the command line for using WhichKey
-      show_keys = true, -- show the currently pressed key and its label as a message in the command line
-      triggers = 'auto', -- automatically setup triggers
+      show_help = true,                                                                 -- show a help message in the command line for using WhichKey
+      show_keys = true,                                                                 -- show the currently pressed key and its label as a message in the command line
+      triggers = 'auto',                                                                -- automatically setup triggers
       -- triggers = {"<leader>"} -- or specifiy a list manually
       -- list of triggers, where WhichKey should not wait for timeoutlen and show immediately
       triggers_nowait = {
