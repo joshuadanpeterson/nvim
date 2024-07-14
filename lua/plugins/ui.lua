ccc1657bc8a8fe0c7cba1959ebc02c13ec3b8a1b
Josh Peterson <joshuadanpeterson@gmail.com>
2024-07-13 23:05:25 -0600

    feat(ui): Update logo and keymaps
    - Changed logo from LazyVim to Neovim in Ascii art
    - Rebuilt Neovim config shortcut
    - Added Tmux and Zsh config shortcuts
    - Add shortcuts to Obsidian Vaults

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -40,13 +40,14 @@ return {
     event = 'VimEnter',
     opts = function()
       local logo = [[
-             ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
-             ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z
-             ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z
-             ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z
-             ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║
-             ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝
-        ]]
+::::    ::: ::::::::::  ::::::::  :::     ::: ::::::::::: ::::    ::::
+:+:+:   :+: :+:        :+:    :+: :+:     :+:     :+:     +:+:+: :+:+:+
+:+:+:+  +:+ +:+        +:+    +:+ +:+     +:+     +:+     +:+ +:+:+ +:+
++#+ +:+ +#+ +#++:++#   +#+    +:+ +#+     +:+     +#+     +#+  +:+  +#+
++#+  +#+#+# +#+        +#+    +#+  +#+   +#+      +#+     +#+       +#+
+#+#   #+#+# #+#        #+#    #+#   #+#+#+#       #+#     #+#       #+#
+###    #### ##########  ########      ###     ########### ###       ###
+      ]]
 
       logo = string.rep('\n', 8) .. logo .. '\n\n'
 
@@ -62,11 +63,93 @@ return {
             { action = 'ene | startinsert', desc = ' New file', icon = ' ', key = 'n' },
             { action = 'Telescope oldfiles', desc = ' Recent files', icon = ' ', key = 'r' },
             { action = 'Telescope live_grep', desc = ' Find text', icon = ' ', key = 'g' },
-            { action = [[lua require("lazyvim.util").telescope.config_files()()]], desc = ' Config', icon = ' ', key = 'c' },
+            {
+              action = function()
+                vim.cmd('cd /Users/joshpeterson/.config/nvim')
+                vim.cmd('edit init.lua')
+                vim.notify("Changed to Neovim config")
+                vim.cmd('source ~/.config/nvim/lua/config/keymaps.lua')
+              end,
+              desc = ' Neovim Config',
+              icon = ' ',
+              key = 'c'
+            },
+
+            {
+              action = function()
+                vim.cmd('cd /Users/joshpeterson/.config/tmux')
+                vim.cmd('edit .tmux.conf')
+                vim.notify("Changed to Tmux config")
+                vim.cmd('source ~/.config/nvim/lua/config/keymaps.lua')
+              end,
+              desc = ' Tmux Config',
+              icon = ' ',
+              key = 't'
+            },
+
+            {
+              action = function()
+                vim.cmd('cd /Users/joshpeterson/.config/zsh-config/')
+                vim.cmd('edit .zshrc')
+                vim.notify("Changed to Zsh config")
+                vim.cmd('source ~/.config/nvim/lua/config/keymaps.lua')
+              end,
+              desc = ' Zsh Config',
+              icon = ' ',
+              key = 'z'
+            },
             { action = 'lua require("persistence").load()', desc = ' Restore Session', icon = ' ', key = 's' },
             { action = 'LazyExtras', desc = ' Lazy Extras', icon = ' ', key = 'x' },
             { action = 'Lazy', desc = ' Lazy', icon = '󰒲 ', key = 'l' },
             { action = 'Leet', desc = ' LeetCode', icon = ' ', key = 'd' },
+
+            {
+              action =
+              [[lua vim.cmd('cd /Users/joshpeterson/Dropbox/DropsyncFiles/Obsidian Vault/Blogging | edit . | lua vim.notify("Changed working directory to Obsidian Blogging Vault"); vim.cmd("source ~/.config/nvim/lua/config/keymaps.lua")')]],
+              desc = ' Open Blogging Vault',
+              icon = ' ',
+              key = 'b'
+            },
+
+            {
+              action =
+              [[lua vim.cmd('cd /Users/joshpeterson/Dropbox/DropsyncFiles/Obsidian Vault/Crypto | edit . | lua vim.notify("Changed working directory to Obsidian Crypto Vault"); vim.cmd("source ~/.config/nvim/lua/config/keymaps.lua")')]],
+              desc = ' Open Crypto Vault',
+              icon = ' ',
+              key = 'C'
+            },
+
+            {
+              action =
+              [[lua vim.cmd('cd /Users/joshpeterson/Dropbox/DropsyncFiles/Obsidian Vault/Journals | edit . | lua vim.notify("Changed working directory to Obsidian Journal Vault"); vim.cmd("source ~/.config/nvim/lua/config/keymaps.lua")')]],
+              desc = ' Open Journal Vault',
+              icon = ' ',
+              key = 'j'
+            },
+
+            {
+              action =
+              [[lua vim.cmd('cd /Users/joshpeterson/Dropbox/DropsyncFiles/Obsidian Vault/Poetry | edit . | lua vim.notify("Changed working directory to Obsidian Poetry Vault"); vim.cmd("source ~/.config/nvim/lua/config/keymaps.lua")')]],
+              desc = ' Open Poetry Vault',
+              icon = ' ',
+              key = 'P'
+            },
+
+            {
+              action =
+              [[lua vim.cmd('cd /Users/joshpeterson/Dropbox/DropsyncFiles/Obsidian Vault/Programming | edit . | lua vim.notify("Changed working directory to Obsidian Programming Vault"); vim.cmd("source ~/.config/nvim/lua/config/keymaps.lua")')]],
+              desc = ' Open Programming Vault',
+              icon = ' ',
+              key = 'p'
+            },
+
+            {
+              action =
+              [[lua vim.cmd('cd /Users/joshpeterson/Dropbox/DropsyncFiles/Obsidian Vault/Udemy/| edit . | lua vim.notify("Changed working directory to Obsidian Udemy Vault"); vim.cmd("source ~/.config/nvim/lua/config/keymaps.lua")')]],
+              desc = ' Open Udemy Vault',
+              icon = ' ',
+              key = 'u'
+            },
             { action = 'qa', desc = ' Quit', icon = ' ', key = 'q' },
           },
           footer = function()
