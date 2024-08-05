f0bdf362c3fe62c9c682494fdb7d4b57765d3445
Josh Peterson <joshuadanpeterson@gmail.com>
2024-08-04 21:20:56 -0500

    refactor(plugins/utility): Fix syntax in Firenvim config

--- lua/plugins/utility.lua
+++ lua/plugins/utility.lua
@@ -284,9 +284,7 @@ return {
     -- Lazy load firenvim
     -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
     lazy = not vim.g.started_by_firenvim,
-    build = function()
-      vim.fn['firenvim#install'](0)
-    end,
+    build = ":call firenvim#install(0)"
   },
 
   -- ranger.nvim file manager
