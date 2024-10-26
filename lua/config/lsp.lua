ef23a41290d40c6468d13df0c56592e48763cfc7
Josh Peterson <joshuadanpeterson@gmail.com>
2024-10-26 13:40:35 -0600

    refactor(config/lsp): Improve LSP configuration structure and readability
    - Reorganized `lsp.lua` for better readability and modularity
    - Grouped LSP setups by type, including TypeScript (`ts_ls`), Lua
    (`lua_ls`), and HTML
    - Updated TypeScript LSP settings for enhanced inlay hints and disabled
    suggestions
    - Configured `neodev.nvim` and `mason-lspconfig` for seamless plugin
    integration
    - Customized diagnostic display settings and added linting autocmds for
    consistency

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,112 +1,73 @@
--- LSP configurations
--- Ensure that the correct filetype is set for your init.lua
+-- Set filetype for init.lua
 vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
   pattern = 'init.lua',
   callback = function()
     vim.bo.filetype = 'lua'
-    -- print 'LSP Filetype set to lua for init.lua'
   end,
 })
 
+-- Load lsp-zero and apply preset
 local lsp_zero = require 'lsp-zero'
 lsp_zero.extend_lspconfig()
-lsp_zero.preset 'recommended' -- firenvim addition
+lsp_zero.preset 'recommended'
 
+-- Attach default keymaps using lsp-zero
 lsp_zero.on_attach(function(client, bufnr)
   lsp_zero.default_keymaps { buffer = bufnr }
 end)
 
-local lspconfig = require 'lspconfig' -- firenvim addition
-local cmp = require 'cmp' -- firenvim addition
+-- Load required plugins
+local lspconfig = require 'lspconfig'
+local cmp = require 'cmp'
 
-lsp_zero.set_preferences { -- firenvim addition
+-- Set lsp-zero preferences
+lsp_zero.set_preferences {
   set_lsp_keymaps = true,
   manage_nvim_cmp = true,
   suggest_lsp_servers = true,
 }
 
--- Enhanced capabilities from nvim-cmp for LSP
-local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-capabilities.textDocument.completion.completionItem.snippetSupport = true -- firenvim addition
--- For emmet-ls
-local emmet_capabilities = vim.lsp.protocol.make_client_capabilities()
-emmet_capabilities.textDocument.completion.completionItem.snippetSupport = true
+-- Enhanced capabilities for nvim-cmp
+local capabilities = require('cmp_nvim_lsp').default_capabilities(
+  vim.lsp.protocol.make_client_capabilities()
+)
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 
--- neodev.nvim setup ahead of LSP config
+-- Setup neodev.nvim for better Lua LSP support
 require('neodev').setup {
-  ui = {
-    border = 'rounded',
-  },
-  library = {
-    plugins = { 'nvim-dap-ui' },
-    types = true,
-  },
+  ui = { border = 'rounded' },
+  library = { plugins = { 'nvim-dap-ui' }, types = true },
 }
 
--- Setup mason to manage LSP installations
+-- Configure mason for LSP installations
 require('mason').setup()
 require('mason-lspconfig').setup {
   ensure_installed = {
-    'lua_ls',
-    'tsserver',
-    'basedpyright',
-    'html',
-    'cssls',
-    'bashls',
-    'rust_analyzer',
-    'gopls',
-    'phpactor',
-    'emmet_ls',
-    'solargraph',
-    'jsonls',
-    'yamlls',
-    'sqls',
-    'dockerls',
-    'vimls',
+    'lua_ls', 'ts_ls', 'basedpyright', 'html', 'cssls',
+    'bashls', 'rust_analyzer', 'gopls', 'phpactor',
+    'emmet_ls', 'solargraph', 'jsonls', 'yamlls',
+    'sqls', 'dockerls', 'vimls'
   },
   handlers = {
-    -- Default handler
     function(server_name)
-      require('lspconfig')[server_name].setup {
-        capabilities = capabilities,
-      }
-      -- print('LSP server ' .. server_name .. ' configured with nvim-cmp capabilities')
+      lspconfig[server_name].setup { capabilities = capabilities }
     end,
-    -- Custom handler for lua_ls
     lua_ls = function()
       local lua_opts = lsp_zero.nvim_lua_ls()
       lua_opts.capabilities = capabilities
       lua_opts.settings = {
         Lua = {
-          runtime = {
-            version = 'LuaJIT',
-            path = vim.split(package.path, ';'),
-          },
-          diagnostics = {
-            globals = { 'vim' },
-          },
-          workspace = {
-            library = vim.api.nvim_get_runtime_file('', true),
-            checkThirdParty = false,
-          },
-          telemetry = {
-            enable = false,
-          },
-          hint = {
-            enable = true, -- necessary
-          },
+          runtime = { version = 'LuaJIT', path = vim.split(package.path, ';') },
+          diagnostics = { globals = { 'vim' } },
+          workspace = { library = vim.api.nvim_get_runtime_file('', true), checkThirdParty = false },
+          telemetry = { enable = false },
+          hint = { enable = true },
         },
       }
-      require('lspconfig').lua_ls.setup(lua_opts)
-      -- print 'Lua LSP configured with nvim-cmp capabilities'
+      lspconfig.lua_ls.setup(lua_opts)
     end,
-    -- Custom handler for tsserver
-    tsserver = function()
-      require('lspconfig').tsserver.setup {
-        -- on_attach = function(client, bufnr)
-        --   client.resolved_capabilities.document_formatting = false
-        -- end,
-        -- cmd = { 'typescript-language-server', '--stdio' },
+    ts_ls = function()
+      lspconfig.ts_ls.setup {
         capabilities = capabilities,
         filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'html' },
         settings = {
@@ -119,67 +80,41 @@ require('mason-lspconfig').setup {
               includeInlayFunctionLikeReturnTypeHints = true,
               includeInlayEnumMemberValueHints = true,
             },
-            javascript = {
-              inlayHints = {
-                includeInlayParameterNameHints = 'all',
-                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-                includeInlayFunctionParameterTypeHints = true,
-                includeInlayVariableTypeHints = true,
-                includeInlayVariableTypeHintsWhenTypeMatchesName = true,
-                includeInlayPropertyDeclarationTypeHints = true,
-                includeInlayFunctionLikeReturnTypeHints = true,
-                includeInlayEnumMemberValueHints = true,
-              },
-            },
-            -- documentFormatting = false,
           },
         },
         init_options = {
           hostInfo = 'neovim',
-          preferences = {
-            quotePreference = 'single',
-            allowIncompleteCompletions = false,
-          },
+          preferences = { quotePreference = 'single', allowIncompleteCompletions = false },
         },
         disableSuggestions = true,
       }
     end,
-    -- Custom handler for html
     html = function()
-      require('lspconfig').html.setup {
-        cmd = { '/System/Volumes/Data/Users/joshpeterson/.nvm/versions/node/v18.12.1/bin/html-languageserver', '--stdio' },
+      lspconfig.html.setup {
+        cmd = { '/path/to/html-languageserver', '--stdio' },
         filetypes = { 'html', 'htmldjango', 'handlebars', 'javascript', 'typescriptreact', 'javascriptreact' },
+        capabilities = capabilities,
         init_options = {
           configurationSection = { 'html', 'css', 'javascript' },
-          embeddedLanguages = {
-            css = true,
-            javascript = true,
-          },
+          embeddedLanguages = { css = true, javascript = true },
           provideFormatter = true,
         },
-        capabilities = capabilities,
       }
     end,
-    -- Custom handler for emmet_ls
     emmet_ls = function()
-      require('lspconfig').emmet_ls.setup {
-        capabilities = emmet_capabilities,
+      lspconfig.emmet_ls.setup {
+        capabilities = require('cmp_nvim_lsp').default_capabilities(),
         filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less', 'javascript' },
         init_options = {
-          html = {
-            options = {
-              -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
-              ['bem.enabled'] = true,
-            },
-          },
+          html = { options = { ['bem.enabled'] = true } },
         },
       }
     end,
   },
 }
 
--- Configure Pyton LSP
-require('lspconfig').basedpyright.setup {
+-- Additional custom LSP configurations
+lspconfig.basedpyright.setup {
   settings = {
     basedpyright = {
       analysis = {
@@ -240,19 +175,19 @@ require('typescript').setup {
 --   -- Do something with the message request or ignore it
 -- end
 
--- Finalize the LSP setup
+-- Finalize LSP setup
 lsp_zero.setup()
 
--- Customizing diagnostic display
+-- Diagnostic customization
 vim.diagnostic.config {
-  virtual_text = false, -- Enable inline diagnostics
-  signs = true, -- Show signs in the sign column
-  underline = true, -- Underline diagnostics
-  update_in_insert = false, -- Update diagnostics in insert mode
-  severity_sort = true, -- Sort diagnostics by severity
+  virtual_text = false,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
   float = {
-    source = 'always', -- Show the source of the diagnostic
-    border = 'rounded', -- Rounded border for floating windows
+    source = 'always',
+    border = 'rounded',
     focusable = true,
   },
 }
@@ -260,7 +195,7 @@ vim.diagnostic.config {
 -- Set border for LspInfo
 require('lspconfig.ui.windows').default_options.border = 'single'
 
--- Set up nvim-lint to integrate with the LSP diagnostics
+-- Custom lint integration
 vim.cmd [[
   augroup NvimLint
     autocmd!
@@ -268,10 +203,7 @@ vim.cmd [[
   augroup END
 ]]
 
--- Set filetype for Google Apps Script files as JavaScript
-vim.cmd(
-  [[
+-- Google Apps Script filetype as JavaScript
+vim.cmd [[
   autocmd BufRead,BufNewFile *script.google.com_*.txt set filetype=javascript
-]],
-  false
-)
+]]
